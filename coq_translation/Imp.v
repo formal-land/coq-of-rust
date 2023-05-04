c5421ecab9706bf5f80f01c60a436acf15228fb9
Daniel Hilst <danielhilst@gmail.com>
2023-05-04 09:24:30 -0300

    Address comments
    * Update Mem definition
    * Update Loop constructor
    * Update Impure constructor

--- coq_translation/Imp.v
+++ coq_translation/Imp.v
@@ -6,6 +6,12 @@ Import ListNotations.
 Open Scope Z_scope.
 Open Scope string_scope.
 
+(** Fail monad placeholder
+
+   Is going to be replaces by the real result monad
+*)
+Parameter result : Set -> Set -> Set.
+
 (** Imp is a imperative language to be used as target language
     for translation from other imperative source languages to Coq, for
     example Rust and Solidity.
@@ -21,24 +27,18 @@ Inductive Ctl_ty : Set :=
 | Panic (** unrecovarable errors *)
 | Exception. (** recovarable errors/user errors *)
 
-
-(** [Mem i a] is the memory indexed by the type i, containing the type a 
-    
-    I think this is not enough to represent memory, each "memory cell"
-    will have its own type, maybe a list of dependent pairs?
-
-*)
-Parameter Mem : Set -> Set -> Set.
+(** [Mem i] is the memory indexed by the type i, it returns an existential type *)
+Parameter Mem : Set -> (forall a : Set, a).
 
 (** [Imp] defines a free monad for a imperative language *)
 Inductive Imp : Set -> Set
   (** Set -> Set is a guess here to be to make it a mappable *)
   := 
 | Pure {a : Set} : a -> Imp a (** pure expressions *)
-| Impure {a : Set} : a -> Imp a
-  (** impure expressions, like function calls that may fail,
-      not sure if I need this *)
-| Loop {a b : Set} : list a -> Imp a (** may loop return values? *)
+| Impure {a e : Set} : a -> Imp (result a e)
+  (** impure expressions, like function calls that may fail *)
+| Loop {a b : Set} : Imp unit -> Imp unit 
+(** It keeps evaluating its parameter until the answer is [Break]. *)
 | Control {a : Set} : Ctl_ty -> a -> Imp a (** return, break, exceptons ... *)
 | ReadMem {a b : Set} : Mem a b -> a -> Imp b (** read from memory *)
 | WriteMem {a b : Set} : Mem a b -> a -> b -> Imp unit (** write to memory *)
