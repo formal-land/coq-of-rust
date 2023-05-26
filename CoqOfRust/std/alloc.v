b42d573afdec10848544ebfaa9caf2c2eebb9e9a
InfiniteEchoes <128528395+InfiniteEchoes@users.noreply.github.com>
2023-05-26 19:19:54 +0800

    Update `alloc.v`

--- CoqOfRust/std/alloc.v
+++ CoqOfRust/std/alloc.v
@@ -39,7 +39,7 @@ Definition System := System.t.
 
 (* ********TRAITS******** *)
 (* 
-[ ] Allocator
+[x] Allocator
 [ ] GlobalAlloc
 *)
 
@@ -61,28 +61,49 @@ Module Allocator.
         &self,
         layout: Layout
     ) -> Result<NonNull<[u8]>, AllocError> { ... }
+    *)
+    allocate_zeroed : ref Self -> Layout -> Result (NonNull u8) AllocError;
+
+    (* 
     unsafe fn grow(
         &self,
         ptr: NonNull<u8>,
         old_layout: Layout,
         new_layout: Layout
     ) -> Result<NonNull<[u8]>, AllocError> { ... }
+    *)
+    grow : ref Self -> NonNull u8 -> Layout -> Layout
+         -> Result (NonNull u8) AllocError;
+
+    (* 
     unsafe fn grow_zeroed(
         &self,
         ptr: NonNull<u8>,
         old_layout: Layout,
         new_layout: Layout
     ) -> Result<NonNull<[u8]>, AllocError> { ... }
+    *)
+    grow_zeroed : ref Self -> NonNull u8 -> Layout -> Layout
+                -> Result (NonNull u8) AllocError;
+
+    (* 
     unsafe fn shrink(
         &self,
         ptr: NonNull<u8>,
         old_layout: Layout,
         new_layout: Layout
     ) -> Result<NonNull<[u8]>, AllocError> { ... }
+    *)
+    shrink : ref Self -> NonNull u8 -> Layout -> Layout
+            -> Result (NonNull u8) AllocError;
+
+    (*
     fn by_ref(&self) -> &Self
        where Self: Sized { ... }
-    
     *)
+    by_ref 
+      `{Sized.Trait Self}
+    : ref Self -> ref Self;
   }.
 End Allocator.
 
