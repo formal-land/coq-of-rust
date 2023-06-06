3a7e601efe5c693612cb7a162129ab013c795cf2
InfiniteEchoes <128528395+InfiniteEchoes@users.noreply.github.com>
2023-06-06 18:56:50 +0800

    Add `cell` and `char`

---
+++ CoqOfRust/std/cell.v
@@ -0,0 +1,15 @@
+Require Import CoqOfRust.lib.lib.
+
+(* ********STRUCTS******** *)
+(* 
+[ ] LazyCell
+[ ] SyncUnsafeCell
+[ ] BorrowError
+[ ] BorrowMutError
+[ ] Cell
+[ ] OnceCell
+[ ] Ref
+[ ] RefCell
+[ ] RefMut
+[ ] UnsafeCell
+*)
