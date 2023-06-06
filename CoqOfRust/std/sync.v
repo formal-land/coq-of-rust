e98577a0f1311877d9acc6da68c2480f5a49889c
InfiniteEchoes <128528395+InfiniteEchoes@users.noreply.github.com>
2023-06-06 18:44:10 +0800

    Add `sync` and `task`

---
+++ CoqOfRust/std/sync.v
@@ -0,0 +1,38 @@
+Require Import CoqOfRust.lib.lib.
+
+(* ********STRUCTS******** *)
+(* 
+[ ] Exclusive
+[ ] LazyLock
+[ ] Arc
+[ ] Barrier
+[ ] BarrierWaitResult
+[ ] Condvar
+[ ] Mutex
+[ ] MutexGuard
+[ ] Once
+[ ] OnceLock
+[ ] OnceState
+[ ] PoisonError
+[ ] RwLock
+[ ] RwLockReadGuard
+[ ] RwLockWriteGuard
+[ ] WaitTimeoutResult
+[ ] Weak
+*)
+
+(* ********ENUMS******** *)
+(* 
+[ ] TryLockError
+*)
+
+(* ********CONSTANTS******** *)
+(* (Deprecated) *)
+
+(* ********TYPE DEFINITIONS******** *)
+(* 
+[ ] LockResult
+[ ] TryLockResult
+*)
+
+
