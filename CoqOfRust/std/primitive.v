ac51665567e1a16e3f453633fff7e741307fc7d7
InfiniteEchoes <128528395+InfiniteEchoes@users.noreply.github.com>
2023-06-08 17:59:03 +0800

    Update `prelude` and `primitive`

---
+++ CoqOfRust/std/primitive.v
@@ -0,0 +1,22 @@
+Require Import CoqOfRust.lib.lib.
+
+(* ********RE-EXPORTS******** *)
+(*
+[ ] bool
+[ ] char
+[ ] f32
+[ ] f64
+[ ] i128
+[ ] i16
+[ ] i32
+[ ] i64
+[ ] i8
+[ ] isize
+[ ] str
+[ ] u128
+[ ] u16
+[ ] u32
+[ ] u64
+[ ] u8
+[ ] usize
+*)
