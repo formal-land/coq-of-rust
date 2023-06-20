0de86668b15a84923ce91319ffaebfd8c394dda5
Daniel Hilst <danielhilst@gmail.com>
2023-06-20 10:42:49 -0300

    Add a more complete example

--- examples/expressions/const_underscore_expression.rs
+++ examples/expressions/const_underscore_expression.rs
@@ -3,3 +3,19 @@ const _: () = {
         test: bool,
     }
 };
+
+struct Bar {
+    test: String,
+}
+
+trait BarTrait {
+    fn show(self) -> String;
+}
+
+const _: () = {
+    impl BarTrait for Bar {
+        fn show(self: Self) -> String {
+            self.test
+        }
+    }
+};
