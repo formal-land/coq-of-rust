f892a47beeb8fc7bdad395541c0c00d6a1b0920c
Bartłomiej Królikowski <bartlomiej.krolikowski@yahoo.com>
2023-07-07 09:36:48 +0200

    added an axiomatic instance of State.Trait

--- CoqOfRust/Monad.v
+++ CoqOfRust/Monad.v
@@ -1,12 +1,15 @@
 (* Imports monad-related definitions *)
-Require MonadExperiments.
+Require experiments.StateMonad.
+
+Parameter State Address : Set.
+Instance H : StateMonad.State.Trait State Address. Admitted.
 
 (** A sketch of the [M] monad *)
-Definition M := MonadExperiments.M : Set -> Set.
+Definition M := StateMonad.M : Set -> Set.
 Definition Pure :=
-  (fun _ => MonadExperiments.pure) : forall {a : Set}, a -> M a.
+  (fun _ => StateMonad.pure) : forall {a : Set}, a -> M a.
 Definition bind :=
-  (fun _ _ => MonadExperiments.bind) :
+  (fun _ _ => StateMonad.bind) :
   forall {a b : Set}, M a -> (a -> M b) -> M b.
 
 (** Used for the definitions of "const". *)
