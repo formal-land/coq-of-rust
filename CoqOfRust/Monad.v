cf341c047788fe84a7adec586cedee7038000dd8
Bartłomiej Królikowski <bartlomiej.krolikowski@yahoo.com>
2023-07-07 09:29:43 +0200

    defined some parameters of module M

--- CoqOfRust/Monad.v
+++ CoqOfRust/Monad.v
@@ -1,7 +1,13 @@
+(* Imports monad-related definitions *)
+Require MonadExperiments.
+
 (** A sketch of the [M] monad *)
-Parameter M : Set -> Set.
-Parameter Pure : forall {a : Set}, a -> M a.
-Parameter bind : forall {a b : Set}, M a -> (a -> M b) -> M b.
+Definition M := MonadExperiments.M : Set -> Set.
+Definition Pure :=
+  (fun _ => MonadExperiments.pure) : forall {a : Set}, a -> M a.
+Definition bind :=
+  (fun _ _ => MonadExperiments.bind) :
+  forall {a b : Set}, M a -> (a -> M b) -> M b.
 
 (** Used for the definitions of "const". *)
 Parameter run : forall {A : Set}, M A -> A.
