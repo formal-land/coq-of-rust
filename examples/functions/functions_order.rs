4dbe5e5c494ba953cc7e601eab75d767c79ce4ec
Daniel Hilst Selli <danielhilst@gmail.com>
2023-07-18 19:26:30 -0300

    Add implenentation to functions_order.rs example

--- examples/functions/functions_order.rs
+++ examples/functions/functions_order.rs
@@ -1,3 +1,10 @@
+pub struct SomeType(u32);
+
+impl SomeType {
+    pub fn meth1(self) { self.meth2(); }
+    fn meth2(self) {}
+}
+
 mod inner_mod {
     pub fn bar() {
 	tar();
@@ -9,6 +16,7 @@ mod inner_mod {
 fn main() {
     foo();
     inner_mod::bar();
+    SomeType(0).meth1();
 }
 
 fn foo() {}
