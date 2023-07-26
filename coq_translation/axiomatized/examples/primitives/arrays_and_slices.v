c725b3b4d18b8e1577ce7e8a49989f0983aaafbe
Bartłomiej Królikowski <bartlomiej.krolikowski@yahoo.com>
2023-07-26 10:08:42 +0200

    updated the axiomatized translation of the examples

--- coq_translation/axiomatized/examples/primitives/arrays_and_slices.v
+++ coq_translation/axiomatized/examples/primitives/arrays_and_slices.v
@@ -2,7 +2,7 @@
 Require Import CoqOfRust.CoqOfRust.
 
 Parameter analyze_slice : forall `{H : State.Trait},
-    ref Slice ->
+    ref (Slice i32) ->
     M (H := H) unit.
 
 (* #[allow(dead_code)] - function was ignored by the compiler *)
