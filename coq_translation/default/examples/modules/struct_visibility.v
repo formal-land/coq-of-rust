7a8045921a4dfd842bae316485435bbe2f41bf03
Daniel Hilst <danielhilst@gmail.com>
2023-08-02 10:04:32 -0300

    Fix compilation

--- coq_translation/default/examples/modules/struct_visibility.v
+++ coq_translation/default/examples/modules/struct_visibility.v
@@ -82,7 +82,7 @@ Module ClosedBox.
 End ClosedBox.
 Definition ClosedBox : Set := @ClosedBox.t.
 
-Module Impl_struct_visibility_my_ClosedBox_T.
+Module Impl_struct_visibility_my_ClosedBox_T_2.
   Definition Self := struct_visibility.my.ClosedBox T.
   
   Definition new
@@ -95,7 +95,7 @@ Module Impl_struct_visibility_my_ClosedBox_T.
     Notation.DoubleColon Self "new" := {
     Notation.double_colon := new;
   }.
-End Impl_struct_visibility_my_ClosedBox_T.
+End Impl_struct_visibility_my_ClosedBox_T_2.
 
 (* #[allow(dead_code)] - function was ignored by the compiler *)
 Definition main `{H : State.Trait} : M (H := H) unit :=
