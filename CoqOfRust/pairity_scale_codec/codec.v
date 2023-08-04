a50cf646e9f2505e0fee3dc09c1f0094ff216f45
Bartłomiej Królikowski <bartlomiej.krolikowski@yahoo.com>
2023-08-04 09:52:46 +0200

    defined trait Output in parity-scale-codec.codec

--- CoqOfRust/pairity_scale_codec/codec.v
+++ CoqOfRust/pairity_scale_codec/codec.v
@@ -1,2 +1,19 @@
 (* the translation of codec.rs form parity-scale-codec *)
-Require CoqOfRust.core.fmt.
+Require Import CoqOfRust.CoqOfRust.
+(*Require CoqOfRust.core.fmt.*)
+
+Module Output.
+  Class Trait (Self : Set) : Set := {
+    write `{State.Trait} : mut_ref Self -> ref (Slice u8) -> M unit;
+  }.
+
+  Global Instance Method_write `{State.Trait} `(Trait) :
+    Notation.Dot "write" := {
+    Notation.dot := write;
+  }.
+
+  Global Instance Method_push_byte `{State.Trait} `(Trait) :
+    Notation.Dot "push_byte" := {
+    Notation.dot (self : mut_ref Self) (byte : u8) := self.["write"] (byte::nil);
+  }.
+End Output.
