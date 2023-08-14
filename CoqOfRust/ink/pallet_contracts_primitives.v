f59b8b7107f918e0373c056e21f837d533162408
InfiniteEchoes <128528395+InfiniteEchoes@users.noreply.github.com>
2023-08-14 11:27:56 +0800

    Initial stub for `pallet_contracts_primitives`

--- CoqOfRust/ink/pallet_contracts_primitives.v
+++ CoqOfRust/ink/pallet_contracts_primitives.v
@@ -1,5 +1,25 @@
 (* Written by hand *)
+Require Import CoqOfRust.CoqOfRust.
+(* Require Import CoqOfRust.lib.lib. *)
+(* NOTE: For now I use the version in `core`, since _std version "makes inconsistent assumptions with CoqOfRust.lib.lib." *)
+Require Import CoqOfRust.core.result.
 
-Module ContractInstantiateResult.
+(* 
+pub struct CodeUploadReturnValue<CodeHash, Balance> {
+    pub code_hash: CodeHash,
+    pub deposit: Balance,
+}
+*)
+Unset Primitive Projections.
+Module CodeUploadReturnValue.
+  Record t (CodeHash Balance : Set): Set := { 
+    code_hash : CodeHash;
+    deposit : Balance;
+  }.
+End CodeUploadReturnValue.
+Global Set Primitive Projections.
+Definition CodeUploadReturnValue := CodeUploadReturnValue.t.
 
-End ContractInstantiateResult.
+(* CodeUploadResult<CodeHash, Balance> = Result<CodeUploadReturnValue<CodeHash, Balance>, DispatchError> *)
+
+Definition CodeUploadResult (CodeHash Balance : Set) := Result (CodeUploadReturnValue (CodeHash Balance) DispatchError).
