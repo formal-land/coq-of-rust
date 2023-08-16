79005302ab5766ae4a86ce9aa6a64247a9b0436b
InfiniteEchoes <128528395+InfiniteEchoes@users.noreply.github.com>
2023-08-16 16:46:58 +0800

    Update `sp_core`

--- CoqOfRust/ink/sp_core.v
+++ CoqOfRust/ink/sp_core.v
@@ -2,8 +2,36 @@
 Require Import CoqOfRust.CoqOfRust.
 
 (* TODO: Implement the followings:
-- [ ] sr25519.Signature
-- [ ] crypto.AccountId32
-- [ ] sr25519.Pair
-- [ ] sr25519.Signature
+- [?] sr25519.Signature
+- [x] crypto.AccountId32
+- [x] sr25519.Pair
 *)
+
+Module sr25519.
+  (* pub struct Pair(_); *)
+  Unset Primitive Projections.
+  Module Pair.
+    Record t : Set := { }.
+  End Pair.
+  Global Set Primitive Projections.
+  Definition Pair := Pair.t.
+
+  (* pub struct Signature(pub [u8; 64]); *)
+  (* NOTE: waiting for a correct implementation in CoqOfRust for this kind of slice... *)
+  Unset Primitive Projections.
+  Module Signature.
+    Record t : Set := { }.
+  End Signature.
+  Global Set Primitive Projections.
+  Definition Signature := Signature.t.
+End sr25519.
+
+Module crypto.
+  (* pub struct AccountId32(_); *)
+  Unset Primitive Projections.
+  Module AccountId32.
+    Record t : Set := { }.
+  End AccountId32.
+  Global Set Primitive Projections.
+  Definition AccountId32 := AccountId32.t.
+End crypto.
