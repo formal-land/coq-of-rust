ff72df3a628589c5d47c8480203a03bae0d05a04
Bartłomiej Królikowski <bartlomiej.krolikowski@yahoo.com>
2023-08-17 14:43:05 +0200

    added num_traits.v

--- CoqOfRust/ink/update_after_translation.py
+++ CoqOfRust/ink/update_after_translation.py
@@ -13,6 +13,8 @@ def update_ink_env():
         sub_exactly_once(
             pattern,
             pattern + """
+Require CoqOfRust.num_traits.
+
 Require CoqOfRust.ink.alloc.
 Require CoqOfRust.ink.ink_primitives.
 Require CoqOfRust.ink.ink_storage_traits.
