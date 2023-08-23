7d61d1d0dc3f76ba24dfc88a33fd027e690e426b
Daniel Hilst <danielhilst@gmail.com>
2023-08-23 08:53:05 -0300

    Add some comments

--- lib/src/reorder.rs
+++ lib/src/reorder.rs
@@ -1,7 +1,4 @@
 /// This module implements the reordering of the definitions in the output
-/// and also the `--generate-reorder` option. The order is read from
-/// the configuration file and applied before the compilation  with
-/// the compile_* functions, and happens at top_level.rs module
 use crate::configuration::*;
 use crate::env::*;
 use itertools::Itertools;
@@ -42,15 +39,19 @@ fn get_name(tcx: &TyCtxt, a_id: HirId) -> String {
 /// configuration file. The reordering happens before the compilation
 /// i.e. before calling the `compile_...` functions, in the
 /// HIR.
+///
+/// In the configuration file:
+///
+/// `{ "reorder": { "somefile.rs": { "area": "height", ...}, ... }, ... }`
+///
+/// This means that "area" should appear after "height". The identifier
+/// in the key is always moved to the value's position.
 #[allow(clippy::ptr_arg)] // Disable warning over &mut Vec<...>, using &mut[...] wont compile
 pub(crate) fn reorder_definitions_inplace(
     tcx: &TyCtxt,
     env: &mut Env,
     definitions: &mut Vec<impl GetHirId>,
 ) {
-    // If we have only one element we still want it to show up
-    // in the output of `--generate-reorder`, even if there
-    // is no sorting going on
     if definitions.is_empty() {
         return;
     }
@@ -60,8 +61,15 @@ pub(crate) fn reorder_definitions_inplace(
             "{}",
             definitions
                 .iter()
-		.enumerate()
-                .map(|(i, def)| format!("\x1b[0;31mreorder before: {i:6} {}/{} {}\x1b[0m", env.file, get_context_name(tcx, &def.hir_id()), get_name(tcx, def.hir_id())))
+                .enumerate()
+                .map(|(i, def)| format!(
+                    // \x1b[0;31m <- red
+                    // \x1b[0m <- reset
+                    "\x1b[0;31mreorder before: {i:6} {}/{} {}\x1b[0m",
+                    env.file,
+                    get_context_name(tcx, &def.hir_id()),
+                    get_name(tcx, def.hir_id())
+                ))
                 .collect::<Vec<String>>()
                 .join("\n")
         );
@@ -69,29 +77,36 @@ pub(crate) fn reorder_definitions_inplace(
 
     let definitions_ids: Vec<HirId> = definitions.iter().map(|def| def.hir_id()).collect();
 
-    // @TODO 
-    // compute the origional position
-
     for def_id in &definitions_ids {
         let def_name = get_name(tcx, *def_id);
         let context = get_context_name(tcx, &def_id);
- 	let pos = definitions_ids.iter().position(|elm| elm == def_id).unwrap();
+        let pos = definitions_ids
+            .iter()
+            .position(|elm| elm == def_id)
+            .unwrap();
 
         match config_get_reorder(env, &context, &def_name) {
-	    Some(config_identifier) => {
-		let config_id_pos = definitions_ids.iter()
-		    .map(|hir_id| get_name(tcx, *hir_id))
-		    .position(|elm| elm == config_identifier);
-		if config_id_pos.is_none() {
-		    continue;
-		}
-		let config_id_pos = config_id_pos.unwrap();
-		let def = definitions.remove(pos);
-		definitions.insert(config_id_pos, def);
-	    },
-	    None => continue,
-	}
-    };
+            Some(config_identifier) => {
+                // found a `def_name: config_identifier in the configuration file
+                // move def_name to the config_identifier position
+                let config_id_pos = definitions_ids
+                    .iter()
+                    .map(|hir_id| get_name(tcx, *hir_id))
+                    .position(|elm| elm == config_identifier);
+                let file = &env.file;
+                if config_id_pos.is_none() {
+                    eprintln!("Warning: No symbol {config_identifier} found in the context {file}/{context}, is this a typo?");
+                    eprintln!("Warning: Enable `debug_reorder` to see the identifiers in {file}/{context} scope");
+                    eprintln!("Warning: Example `cargo coq-of-rust 2>&1 | grep -e 'reorder before: .* {file}/{context} '`");
+                    continue;
+                }
+                let config_id_pos = config_id_pos.unwrap();
+                let def = definitions.remove(pos);
+                definitions.insert(config_id_pos, def);
+            }
+            None => continue,
+        }
+    }
 
     if env.configuration.debug_reorder {
         eprintln!(
@@ -99,30 +114,21 @@ pub(crate) fn reorder_definitions_inplace(
             definitions
                 .iter()
                 .map(|def| {
-		    let id = def.hir_id();
-		    let pos = definitions_ids.iter().position(|x| *x == id).unwrap_or(0);
-		    format!("\x1b[0;32mreorder after:  {pos:6} {}/{} {}\x1b[0m", env.file, get_context_name(tcx, &def.hir_id()), get_name(tcx, def.hir_id()))
-		})
+                    let id = def.hir_id();
+                    let pos = definitions_ids.iter().position(|x| *x == id).unwrap_or(0);
+                    format!(
+                        // \x1b[0;32m <- green
+                        // \x1b[0m <- reset
+                        "\x1b[0;32mreorder after:  {pos:6} {}/{} {}\x1b[0m",
+                        env.file,
+                        get_context_name(tcx, &def.hir_id()),
+                        get_name(tcx, def.hir_id())
+                    )
+                })
                 .collect::<Vec<String>>()
                 .join("\n")
         );
     }
-
-    let identifiers = definitions
-        .iter()
-        .filter_map(|def| {
-            let name = get_name(tcx, def.hir_id());
-            if !name.is_empty() && name != "test" && name != "std" {
-                Some(name)
-            } else {
-                None
-            }
-        })
-        .unique()
-        .collect::<Vec<String>>();
-
-    let context = get_context_name(tcx, &definitions[0].hir_id());
-    env.reorder_map.insert(context, identifiers);
 }
 
 /// Extract the type name for a node if is a trait
