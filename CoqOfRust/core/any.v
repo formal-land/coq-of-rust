8ad5a86ad8cc51172b1d9ef18362e2b6c5070a89
Bartłomiej Królikowski <bartlomiej.krolikowski@yahoo.com>
2023-08-23 15:01:24 +0200

    restored previous content of core/any.v

--- CoqOfRust/core/any.v
+++ CoqOfRust/core/any.v
@@ -1,10 +1,49 @@
 Require Import CoqOfRust.lib.lib.
-Require CoqOfRust.core.any.
 
-Module Demand := core.any.Demand.
-Module TypeId := core.any.TypeId.
-Module Provider := core.any.Provider.
-Module Any := core.any.Any.
+(* ********STRUCTS******** *)
+(* 
+[x] Demand
+[x] TypeId 
+*)
 
-Definition Demand := core.any.Demand.
-Definition TypeId := core.any.TypeId.
+(* pub struct Demand<'a>(_); *)
+Module Demand.
+  Parameter t : Set.
+End Demand.
+Definition Demand := Demand.t.
+
+(* pub struct TypeId { /* private fields */ } *)
+Module TypeId.
+  Parameter t : Set.
+End TypeId.
+Definition TypeId := TypeId.t.
+
+(* ********TRAITS******** *)
+(* 
+[x] Provider
+[x] Any 
+*)
+
+(* 
+pub trait Provider {
+    // Required method
+    fn provide<'a>(&'a self, demand: &mut Demand<'a>);
+}
+*)
+Module Provider.
+  Class Trait (Self : Set) : Set := { 
+    provide : ref Self -> mut_ref Demand;
+  }.
+End Provider.
+
+(* 
+pub trait Any: 'static {
+    // Required method
+    fn type_id(&self) -> TypeId;
+}
+*)
+Module Any.
+  Class Trait (Self : Set) : Set := { 
+    type_id : ref Self -> TypeId;
+  }.
+End Any.
