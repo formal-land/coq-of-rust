95febacd6dd6d8df62f3fce83cfdbe15138f44c8
Daniel Hilst <danielhilst@gmail.com>
2023-08-28 15:48:54 -0300

    Update ink .v files

--- CoqOfRust/ink/ink_e2e_macro.v
+++ CoqOfRust/ink/ink_e2e_macro.v
@@ -2,6 +2,18 @@
 Require Import CoqOfRust.CoqOfRust.
 Require CoqOfRust.ink.proc_macro.
 Require CoqOfRust.ink.syn.
+Require CoqOfRust.ink.proc_macro.
+Require CoqOfRust.ink.syn.
+Require CoqOfRust.ink.proc_macro.
+Require CoqOfRust.ink.syn.
+Require CoqOfRust.ink.proc_macro.
+Require CoqOfRust.ink.syn.
+Require CoqOfRust.ink.proc_macro.
+Require CoqOfRust.ink.syn.
+Require CoqOfRust.ink.proc_macro.
+Require CoqOfRust.ink.syn.
+Require CoqOfRust.ink.proc_macro.
+Require CoqOfRust.ink.syn.
 
 Module codegen.
   Parameter ALREADY_BUILT_CONTRACTS :
