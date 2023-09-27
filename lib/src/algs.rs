0fd0be28070be3fbf96d876492c4c71071b1d73e
Bartłomiej Królikowski <bartlomiej.krolikowski@yahoo.com>
2023-09-27 10:59:11 +0200

    add a function in in algs.rs

--- lib/src/algs.rs
+++ lib/src/algs.rs
@@ -2,6 +2,42 @@ use std::collections::{HashMap, HashSet, VecDeque};
 
 type DirectedGraph<V> = HashMap<V, HashSet<V>>;
 
+#[inline]
+/// assigns `new` to `*old` if `*old < new`
+fn assign_if_greater<A>(old: &mut A, new: A)
+where
+    A: std::cmp::PartialOrd,
+{
+    if *old < new {
+        *old = new
+    }
+}
+
+pub(crate) fn longest_paths_length<'a, V>(start: &'a V, graph: &'a DirectedGraph<V>) -> HashMap<&'a V, u32>
+where
+    V: core::clone::Clone + std::cmp::Eq + std::hash::Hash,
+{
+    let mut distances = HashMap::new();
+    let mut queue = VecDeque::new();
+    queue.push_back((start, 0u32));
+
+    while let Some((current, dist)) = queue.pop_front() {
+        if let Some(vertices) = graph.get(current) {
+            for v in vertices {
+                let new_dist = dist + 1;
+                distances
+                    .entry(v)
+                    .and_modify(|old_dist| assign_if_greater(old_dist, new_dist))
+                    .or_insert(new_dist);
+
+                queue.push_back((v, new_dist));
+            }
+        }
+    }
+
+    distances
+}
+
 pub(crate) fn find_last<V>(start: V, graph: DirectedGraph<V>) -> V
 where
     V: core::clone::Clone + std::cmp::Eq + std::hash::Hash,
