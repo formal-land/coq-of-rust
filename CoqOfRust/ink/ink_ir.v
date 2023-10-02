b5378fe027610597082f4fc967cd84cee568349e
Guillaume Claret <dev@clarus.me>
2023-10-02 10:49:39 +0200

    ci: translate ink_e2e_macro.v

--- CoqOfRust/ink/ink_ir.v
+++ CoqOfRust/ink/ink_ir.v
@@ -1,5 +1,11 @@
 Require Import CoqOfRust.CoqOfRust.
 
+Module ast.
+  Module attr_args.
+    Parameter AttributeArgs : Set.
+  End attr_args.
+End ast.
+
 Module ir.
   Module utils.
     Parameter WhitelistedAttributes : Set.
