a69f67c73cfc146616cb415937550a305ae8383e
InfiniteEchoes <128528395+InfiniteEchoes@users.noreply.github.com>
2023-12-29 18:55:19 +0800

    Redo the change

--- CoqOfRust/examples/axiomatized/examples/types/literals.v
+++ CoqOfRust/examples/axiomatized/examples/types/literals.v
@@ -5,19 +5,19 @@ Require Import CoqOfRust.CoqOfRust.
 fn main() {
     // Suffixed literals, their types are known at initialization
     let x = 1u8;
-    // let y = 2u32;
-    // let z = 3f32;
+    let y = 2u32;
+    let z = 3f32;
 
     // Unsuffixed literals, their types depend on how they are used
-    // let i = 1;
-    // let f = 1.0;
+    let i = 1;
+    let f = 1.0;
 
     // `size_of_val` returns the size of a variable in bytes
     println!("size of `x` in bytes: {}", std::mem::size_of_val(&x));
-    // println!("size of `y` in bytes: {}", std::mem::size_of_val(&y));
-    // println!("size of `z` in bytes: {}", std::mem::size_of_val(&z));
-    // println!("size of `i` in bytes: {}", std::mem::size_of_val(&i));
-    // println!("size of `f` in bytes: {}", std::mem::size_of_val(&f));
+    println!("size of `y` in bytes: {}", std::mem::size_of_val(&y));
+    println!("size of `z` in bytes: {}", std::mem::size_of_val(&z));
+    println!("size of `i` in bytes: {}", std::mem::size_of_val(&i));
+    println!("size of `f` in bytes: {}", std::mem::size_of_val(&f));
 }
 *)
 (* #[allow(dead_code)] - function was ignored by the compiler *)
