4474c859610b2799bfcedce784532c3c1d2000eb
Valerii Huhnin <olympichek1@gmail.com>
2024-02-20 17:31:10 +0000

    translate match expressions to `M.monadic_match_operator` tactic

--- lib/src/thir_expression.rs
+++ lib/src/thir_expression.rs
@@ -645,32 +645,40 @@ fn build_match(scrutinee: Rc<Expr>, arms: Vec<MatchArm>, _ty: Option<Rc<CoqType>
     });
 
     Rc::new(ExprKind::Call {
-        func: Expr::local_var("match_operator"),
-        args: vec![
-            scrutinee,
-            Rc::new(Expr {
-                kind: Rc::new(ExprKind::Array {
-                    elements: arms_with_flatten_patterns
-                        .map(|MatchArm { pattern, body }| {
-                            Rc::new(Expr {
-                                kind: Rc::new(ExprKind::Lambda {
-                                    args: vec![("γ".to_string(), None)],
-                                    body: build_inner_match(
-                                        vec![("γ".to_string(), pattern)],
-                                        body,
-                                        0,
-                                    ),
-                                    is_for_match: true,
-                                }),
-                                ty: None,
-                            })
-                        })
-                        .collect(),
-                }),
-                ty: None,
+        func: Expr::local_var("ltac:"),
+        args: vec![Rc::new(Expr {
+            kind: Rc::new(ExprKind::Call {
+                func: Expr::local_var("M.monadic_match_operator"),
+                args: vec![
+                    scrutinee.clone(),
+                    Rc::new(Expr {
+                        kind: Rc::new(ExprKind::Array {
+                            elements: arms_with_flatten_patterns
+                                .map(|MatchArm { pattern, body }| {
+                                    Rc::new(Expr {
+                                        kind: Rc::new(ExprKind::Lambda {
+                                            args: vec![("γ".to_string(), scrutinee.ty.clone())],
+                                            body: build_inner_match(
+                                                vec![("γ".to_string(), pattern)],
+                                                body,
+                                                0,
+                                            ),
+                                            is_for_match: true,
+                                        }),
+                                        ty: None,
+                                    })
+                                })
+                                .collect(),
+                        }),
+                        ty: None,
+                    }),
+                ],
+                purity: Purity::Pure,
+                from_user: false,
             }),
-        ],
-        purity: Purity::Effectful,
+            ty: None,
+        })],
+        purity: Purity::Pure,
         from_user: false,
     })
 }
