3503487c29642d64d636e2466e58b4e252e09412
Valerii Huhnin <olympichek1@gmail.com>
2024-02-20 12:42:08 +0000

    `MonadicNotationExample.v`: add an example with match expressions
    Also remove an example of an old translation result

--- CoqOfRust/MonadicNotationExample.v
+++ CoqOfRust/MonadicNotationExample.v
@@ -3,51 +3,21 @@ Require Import CoqOfRust.CoqOfRust.
 (* An example of the new monadic notation *)
 Module MonadicNotationExample.
 
-  (* Example of a Rust program *)
   (*
-    fn test() -> u32 {
+  fn test() -> u32 {
       let x = 5u32;
 
       let y = {
-        let x_squared = x * x;
-        let x_cube = x_squared * x;
-        x_cube + x_squared + x
+          let x_squared = x * x;
+          let x_cube = x_squared * x;
+          x_cube + x_squared + x
       };
 
       y + 2
-    }
+  }
   *)
-
-  (* Translation result with explicit names for intermediate results *)
   Definition test : M u32.t :=
-    let* x : M.Val u32.t := M.alloc ((Integer.of_Z 5) : u32.t) in
-    let* y : M.Val u32.t :=
-      let* x_squared : M.Val u32.t :=
-        let* α0 : u32.t := M.read x in
-        let* α1 : u32.t := M.read x in
-        let* α2 : u32.t := BinOp.Panic.mul α0 α1 in
-        M.alloc α2 in
-      let* x_cube : M.Val u32.t :=
-        let* α0 : u32.t := M.read x_squared in
-        let* α1 : u32.t := M.read x in
-        let* α2 : u32.t := BinOp.Panic.mul α0 α1 in
-        M.alloc α2 in
-      let* α0 : u32.t := M.read x_cube in
-      let* α1 : u32.t := M.read x_squared in
-      let* α2 : u32.t := BinOp.Panic.add α0 α1 in
-      let* α3 : u32.t := M.read x in
-      let* α4 : u32.t := BinOp.Panic.add α2 α3 in
-      let* α0 : M.Val u32.t := M.alloc α4 in
-      M.copy α0 in
-    let* α0 : u32.t := M.read y in
-    let* α1 : u32.t := BinOp.Panic.add α0 ((Integer.of_Z 2) : u32.t) in
-    let* α0 : M.Val u32.t := M.alloc α1 in
-    M.read α0.
-
-  (* Representation without explicit names for intermediate results
-     using [M.run] markers and [M.monadic] tactic *)
-  Definition test_new : M u32.t :=
-    ltac:(monadic (
+    ltac:(M.monadic (
       M.read (|
         let x : M.Val u32.t := M.alloc (| (Integer.of_Z 5) : u32.t |) in
         let y : M.Val u32.t :=
@@ -71,7 +41,62 @@ Module MonadicNotationExample.
       |)
     )).
 
-  (* Run to see the transltion result *)
-  (* Print test_new. *)
+  (*
+  fn test_match(x: u32, boolean: bool) -> u32 {
+      let y = match x {
+          1 => 2,
+          3 => 6,
+          5 => 10,
+          7 => 14,
+          _ => 0,
+      };
+
+      let binary = match boolean {
+          false => 0,
+          true => 1,
+      };
+
+      x + y * binary
+  }
+  *)
+  Definition test_match (x : u32.t) (boolean : bool.t) : M u32.t :=
+    ltac:(M.monadic (
+      let x := M.alloc (| x |) in
+      let boolean := M.alloc (| boolean |) in
+      M.read (|
+        let y : M.Val u32.t :=
+          M.copy (|
+            ltac: (M.monadic_match_operator x
+              [
+                fun (γ : M.Val u32.t) =>
+                  (M.alloc (| (Integer.of_Z 2) : u32.t |)) : M.Val u32.t;
+                fun (γ : M.Val u32.t) =>
+                  (M.alloc (| (Integer.of_Z 6) : u32.t |)) : M.Val u32.t;
+                fun (γ : M.Val u32.t) =>
+                  (M.alloc (| (Integer.of_Z 10) : u32.t |)) : M.Val u32.t;
+                fun (γ : M.Val u32.t) =>
+                  (M.alloc (| (Integer.of_Z 14) : u32.t |)) : M.Val u32.t;
+                fun (γ : M.Val u32.t) =>
+                  (M.alloc (| (Integer.of_Z 0) : u32.t |)) : M.Val u32.t
+              ])
+          |) in
+        let binary : M.Val u32.t :=
+          M.copy (|
+          ltac: (M.monadic_match_operator boolean
+              [
+                fun (γ : M.Val u32.t) =>
+                  (M.alloc (| (Integer.of_Z 0) : u32.t |)) : M.Val u32.t;
+                fun (γ : M.Val u32.t) =>
+                  (M.alloc (| (Integer.of_Z 1) : u32.t |)) : M.Val u32.t
+              ])
+          |) in
+        M.alloc (|
+          BinOp.Panic.add (|
+            M.read (| x |),
+            BinOp.Panic.mul (| M.read (| y |), M.read (| binary |) |)
+          |)
+        |)
+      |)
+    )).
 
 End MonadicNotationExample.
