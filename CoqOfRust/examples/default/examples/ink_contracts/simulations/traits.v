377ef39aa35754476b58d5dde7fd33e3c72e9683
InfiniteEchoes <128528395+InfiniteEchoes@users.noreply.github.com>
2024-05-03 13:38:16 +0800

    Update `traits.v`

--- CoqOfRust/examples/default/examples/ink_contracts/simulations/traits.v
+++ CoqOfRust/examples/default/examples/ink_contracts/simulations/traits.v
@@ -1,15 +1,19 @@
 (* traits/traits.rs *)
+
+(* Questions:
+1. Should I translate reference types to Pointer types? It looks like it adds a lot of complexity
+*)
+
+(* TODO:
+1. Distinguish between functions that read the variables and functions that also writes to it
+2. Identify the way to translate them
+
+*)
+
 (* struct Sheep {
   naked: bool,
   name: &'static str,
 } *)
-
-(* TODO: Apply definition of Pointer.t
-  Inductive t (Value : Set) : Set :=
-  | Immediate (value : Value)
-  | Mutable {Address : Set} (address : Address) (path : Path.t).
-*)
-
 Module Sheep.
   Record t : Set := {
     naked: bool,
@@ -24,68 +28,47 @@ Module Sheep.
         ("name", φ x.(name));
       ];
   }.
-
-      (* 
-      fn new(name: &'static str) -> Sheep {
-        Sheep {
-            name: name,
-            naked: false,
-        }
-      } *)
-    (* NOTE: Is this the correct way to construct record in Coq? *)
-    Definition new (name: Pointer.t string) : MS? traits.Sheep.t := 
-      returnS? {| naked := false;
-        name := Pointer.t.Immediate (Value.t.String name);
-      |}.
-
-    (*   
-      fn name(&self) -> &'static str {
-        self.name
-      }
-    *)
-    (* NOTE: How to extract record entries from a record that is wrapped in Pointer.t type? *)
-    Definition name (self: Pointer.t traits.Sheep.t) : MS? (Pointer.t ) := 
-      returnS? (Pointer.t.Immediate (Value.t.String self.name)).
-    (* 
-    fn noise(&self) -> &'static str {
-        if self.is_naked() {
-            "baaaaah?"
-        } else {
-            "baaaaah!"
-        }
-    } *)
-    Definition noise (self: Pointer.t traits.Sheep.t) : MS? (Pointer.t str) := 
-      returnS? (Pointer.t.Immediate (Value.t.String (if is_naked(self) then "baaaaah?" else "baaaaah!"))).
-
-    (* NOTE: unimplemented since it involves println *)
-    (* fn talk(&self) {
-        // For example, we can add some quiet contemplation.
-        println!("{} pauses briefly... {}", self.name, self.noise());
-    } *)
-    Definition talk (self: Pointer.t traits.Sheep.t): MS? unit := returnS? tt.
-    
-    (*
-    impl Sheep {
-      fn shear(&mut self) {
-          if self.is_naked() {
-              // Implementor methods can use the implementor's trait methods.
-              println!("{} is already naked...", self.name());
-          } else {
-              println!("{} gets a haircut!", self.name);
-
-              self.naked = true;
-          }
-      }
-    }
-    *)
-    (* 
-    TODO: apply RecordUpdate to simulate the update of the variable?
-    *)
-    Definition shear (self: traits.Sheep.t) : MS? State unit := 
-      returnS? (if is_naked(self) then tt else tt).
-
-    (* TODO: Make a `IsOfTrait` or something to indicate that Sheep is of Animal trait *)
 End Sheep.
+
+(*  ** Simulation of functions ** *)
+(* 
+fn new(name: &'static str) -> Sheep {
+  Sheep {
+      name: name,
+      naked: false,
+  }
+} *)
+(* NOTE: Is this the correct way to construct record in Coq? *)
+Definition new (name: String) : traits.Sheep.t := 
+  {| naked := false;
+    name := name;
+  |}.
+
+(*   
+fn name(&self) -> &'static str {
+  self.name
+}
+*)
+Definition name (self: traits.Sheep.t) : String := 
+  self.name.
+
+(* 
+fn noise(&self) -> &'static str {
+    if self.is_naked() {
+        "baaaaah?"
+    } else {
+        "baaaaah!"
+    }
+} *)
+Definition noise (self: traits.Sheep.t) : String := 
+  if is_naked(self) then "baaaaah?" else "baaaaah!".
+
+(* NOTE: unimplemented since it involves println *)
+(* fn talk(&self) {
+    // For example, we can add some quiet contemplation.
+    println!("{} pauses briefly... {}", self.name, self.noise());
+} *)
+Definition talk (self: traits.Sheep.t): unit := tt.
 
 (*
 impl Sheep {
@@ -96,6 +79,31 @@ impl Sheep {
 *)
 Definition is_naked (self: traits.Sheep.t) : bool :=
   self.naked.
+
+(* Simulation of a function that modifies a variable *)
+
+Module State.
+  Definition t : Set := traits.Sheep.t.
+End State.
+
+(*
+impl Sheep {
+  fn shear(&mut self) {
+      if self.is_naked() {
+          // Implementor methods can use the implementor's trait methods.
+          println!("{} is already naked...", self.name());
+      } else {
+          println!("{} gets a haircut!", self.name);
+
+          self.naked = true;
+      }
+  }
+}
+*)
+(* TODO: Finish this function *)
+Definition shear (self: traits.Sheep.t) : unit := 
+  letS? '(storage) := readS? in
+  if is_naked(self) then tt else tt.
 
 (*
 trait Animal {
@@ -115,13 +123,14 @@ trait Animal {
 
 Module Animal.
   Class Trait (Self : Set) : Set := {
-    new (name: Pointer.t str) : MS? traits.Sheep.t; 
-    name (self: Pointer.t traits.Sheep.t) : MS? (Pointer.t str);
-    noise (self: Pointer.t traits.Sheep.t) : MS? (Pointer.t str);
-    talk (self: Pointer.t traits.Sheep.t) : MS? unit;
+    new (name: String) : traits.Sheep.t; 
+    name (self: traits.Sheep.t) : String;
+    noise (self: traits.Sheep.t) : String;
+    talk (self: traits.Sheep.t) : unit;
   }.
 
-  (* TODO: Add the IsTrait method here? *)
+  (* TODO: Define the `TraitHasRun` struct to express that `Sheep` implements `Animal` *)
+
 End Animal.
 
 (*
@@ -138,8 +147,8 @@ fn main() {
 Definition main : 
   MS? State.t unit := 
   (* Is this notation still in use? *)
-  let dolly := traits.Animal::["new"] "Dolly" in
+  (* let dolly := traits.Animal::["new"] "Dolly" in
   let _ := dolly::["talk"] in
   let _ := dolly::["shear"] in
-  let _ := dolly::["talk"] in
+  let _ := dolly::["talk"] in *)
   returnS? tt.
