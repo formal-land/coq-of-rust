f443776700a0e600ef3d6d86b0ff2b6e2a680bc2
InfiniteEchoes <128528395+InfiniteEchoes@users.noreply.github.com>
2024-05-05 17:21:54 +0800

    Compiler-checked `constants.v`

--- CoqOfRust/examples/default/examples/ink_contracts/simulations/constants.v
+++ CoqOfRust/examples/default/examples/ink_contracts/simulations/constants.v
@@ -1,19 +1,19 @@
+(* custom_type/constants.v *)
 Require Import CoqOfRust.CoqOfRust.
 Require CoqOfRust.core.simulations.default.
 Require CoqOfRust.core.simulations.option.
 Require CoqOfRust.examples.default.examples.ink_contracts.simulations.lib.
 Require Import CoqOfRust.simulations.M.
+Require Import CoqOfRust.lib.simulations.lib.
 
 Import simulations.M.Notations.
-(* custom_type/constants.v *)
 
 (*
 static LANGUAGE: &str = "Rust";
 const THRESHOLD: i32 = 10;
 *)
-
 Definition LANGUAGE := mk_str "Rust".
-Definition THRESHOLD := i32.Make 10.
+Definition THRESHOLD : i32.t := i32.Make 10.
 
 (* 
 fn is_big(n: i32) -> bool {
@@ -22,16 +22,17 @@ fn is_big(n: i32) -> bool {
 }
 *)
 
-(* TODO: figure out how to compare i32 *)
 Definition is_big 
-  (n: i32.t) : bool := n >? THRESHOLD.
+  (n: i32.t) : bool := 
+  let get_n := i32.get n in
+  let get_THRESHOLD := i32.get THRESHOLD in
+  get_n >? get_THRESHOLD.
 
 (*
 fn main() {
     let n = 16;
 } *)
 
-Definition main : MS? unit :=
+Definition main : unit :=
   let n := i32.Make 16 in
-  returnS? tt.
-  
+  tt.
