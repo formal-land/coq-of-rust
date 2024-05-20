13c8b8a726f02dfa93eefae55ee6f39f45a54059
Valerii Huhnin <olympichek1@gmail.com>
2024-05-20 18:52:52 +0000

    simulation for the `record_cost()` function
    Simulation for the `record_cost()` function that is used in
    the implementation of the `gas!` macro that is used in arithmetic

--- CoqOfRust/revm/simulations/arithmetic.v
+++ CoqOfRust/revm/simulations/arithmetic.v
@@ -46,6 +46,34 @@ Module Gas.
         ("refunded", φ x.(refunded))
       ];
   }.
+
+  (*
+    /// Records an explicit cost.
+    ///
+    /// Returns `false` if the gas limit is exceeded.
+    #[inline]
+    #[must_use = "prefer using `gas!` instead to return an out-of-gas error on failure"]
+    pub fn record_cost(&mut self, cost: u64) -> bool {
+        let (remaining, overflow) = self.remaining.overflowing_sub(cost);
+        let success = !overflow;
+        if success {
+            self.remaining = remaining;
+        }
+        success
+    }
+  *)
+  Definition record_cost
+      (cost : Z) :
+      MS? t string bool :=
+    letS? 'gas := readS? in
+    let remaining_sub_cost := gas.(remaining) - cost in
+    let success := remaining_sub_cost >=? 0 in
+    letS? _ :=
+      if success 
+      then writeS? (gas <| remaining := remaining_sub_cost |>)
+      else returnS? tt
+    in
+    returnS? success.
 End Gas.
 
 (*
