739f0638d282e3a0eb8ee227454a939f0db6e67d
InfiniteEchoes <128528395+InfiniteEchoes@users.noreply.github.com>
2024-06-29 20:01:25 +0800

    minor: update

---
+++ CoqOfRust/move_sui/simulations/move_binary_format/errors.v
@@ -0,0 +1,10 @@
+Require Import CoqOfRust.CoqOfRust.
+Require Import CoqOfRust.simulations.M.
+Require Import CoqOfRust.lib.lib.
+
+Import simulations.M.Notations.
+
+(* TODO: Implement this *)
+(* pub type PartialVMResult<T> = ::std::result::Result<T, PartialVMError>; *)
+Module PartialVMResult.
+End PartialVMResult.
