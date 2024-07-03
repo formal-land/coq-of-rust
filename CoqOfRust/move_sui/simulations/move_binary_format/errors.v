1c604bf9c1103d68ac059ad04f63a159136584a1
InfiniteEchoes <128528395+InfiniteEchoes@users.noreply.github.com>
2024-07-03 12:24:32 +0800

    minor: comment

--- CoqOfRust/move_sui/simulations/move_binary_format/errors.v
+++ CoqOfRust/move_sui/simulations/move_binary_format/errors.v
@@ -4,7 +4,25 @@ Require Import CoqOfRust.lib.lib.
 
 Import simulations.M.Notations.
 
-(* TODO: How to write `std::Result`? *)
+(* TODO: Use the Result type correctly *)
+(* Example code
+Module Result.
+  Inductive t (A Error : Set) : Set :=
+  | Ok : A -> t A Error
+  | Err : Error -> t A Error.
+
+  Arguments Ok {A Error}%type_scope.
+  Arguments Err {A Error}%type_scope.
+
+  Definition return_ {A Error : Set} (value : A) : t A Error := Ok value.
+
+  Definition bind {Error A B : Set} (value : t A Error) (f : A -> t B Error) : t B Error :=
+    match value with
+    | Ok value => f value
+    | Err error => Err error
+    end.
+End Result.
+*)
 (* pub type PartialVMResult<T> = ::std::result::Result<T, PartialVMError>; *)
 Module PartialVMResult.
   Definition t (T : Set) : Set. Admitted.
--- CoqOfRust/move_sui/simulations/move_bytecode_verifier/type_safety.v
+++ CoqOfRust/move_sui/simulations/move_bytecode_verifier/type_safety.v
@@ -62,26 +62,6 @@ End Meter.
 (* TODO: use the notation properly *)
 Definition test_0 : forall (A : Set), { _ : Set @ Meter.Trait A } -> A -> Set. Admitted.
 
-(* TODO: Use the Result type correctly *)
-(* Example code
-Module Result.
-  Inductive t (A Error : Set) : Set :=
-  | Ok : A -> t A Error
-  | Err : Error -> t A Error.
-
-  Arguments Ok {A Error}%type_scope.
-  Arguments Err {A Error}%type_scope.
-
-  Definition return_ {A Error : Set} (value : A) : t A Error := Ok value.
-
-  Definition bind {Error A B : Set} (value : t A Error) (f : A -> t B Error) : t B Error :=
-    match value with
-    | Ok value => f value
-    | Err error => Err error
-    end.
-End Result.
-*)
-
 (* struct Locals<'a> {
     param_count: usize,
     parameters: &'a Signature,
