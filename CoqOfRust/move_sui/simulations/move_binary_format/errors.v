8feb39a983b20c91e310d6b95612a27888bbad97
InfiniteEchoes <128528395+InfiniteEchoes@users.noreply.github.com>
2024-07-03 18:34:31 +0800

    feat: implement `PartialVMError`

--- CoqOfRust/move_sui/simulations/move_binary_format/errors.v
+++ CoqOfRust/move_sui/simulations/move_binary_format/errors.v
@@ -5,6 +5,38 @@ Require Import CoqOfRust.lib.lib.
 Import simulations.M.Notations.
 
 (* TODO: Implement PartialVMError *)
+(* 
+#[derive(Clone)]
+pub struct PartialVMError(Box<PartialVMError_>);
+
+#[derive(Clone)]
+struct PartialVMError_ {
+    major_status: StatusCode,
+    sub_status: Option<u64>,
+    message: Option<String>,
+    exec_state: Option<ExecutionState>,
+    indices: Vec<(IndexKind, TableIndex)>,
+    offsets: Vec<(FunctionDefinitionIndex, CodeOffset)>,
+}
+*)
+Module PartialVMError_.
+  Record t : Set := {
+  (* TODO: Implement below
+    major_status: StatusCode;
+    sub_status: Option<u64>;
+    message: Option<String>;
+    exec_state: Option<ExecutionState>;
+    indices: Vec<(IndexKind, TableIndex)>;
+    offsets: Vec<(FunctionDefinitionIndex, CodeOffset)>;
+  *)
+  }.
+End PartialVMError_.
+
+Module PartialVMError.
+  Record t : Set := {
+    _ : PartialVMError_.t;
+  }.
+End PartialVMError.
 
 (* TODO: Use the Result type correctly *)
 (* Example code
@@ -27,7 +59,7 @@ End Result.
 *)
 (* pub type PartialVMResult<T> = ::std::result::Result<T, PartialVMError>; *)
 Module PartialVMResult.
-  Definition t (T : Set) := Result.t T PartialVMError.
+  Definition t (T : Set) := Result.t T PartialVMError.t..
   Module Impl_move_sui_simulations_move_binary_format_error_PartialVMResult.
     (* TODO: Implement `new` function *)
     Definition new : Set. Admitted.
