988d9b87955cc86db22ad3c608252af21616b608
Valerii Huhnin <olympichek1@gmail.com>
2024-07-05 04:11:57 +0000

    simulation for Move `push_n` function

--- CoqOfRust/move_sui/simulations/move_abstract_stack/lib.v
+++ CoqOfRust/move_sui/simulations/move_abstract_stack/lib.v
@@ -1,5 +1,7 @@
 Require Import CoqOfRust.CoqOfRust.
 Require Import CoqOfRust.simulations.M.
+Require Import CoqOfRust.core.simulations.integers.
+Require Import CoqOfRust.core.simulations.vector.
 Import simulations.M.Notations.
 
 (*
@@ -33,6 +35,16 @@ Module AbstractStack.
     len : Z;
   }.
 
+  Arguments values {A}%type_scope.
+  Arguments len {A}%type_scope.
+
+  Module Lens.
+    Definition values {A : Set} : Lens.t (t A) (list (Z * A)) := {|
+      Lens.read stack := values stack;
+      Lens.write stack vs := stack <| values := vs |>
+    |}.
+  End Lens.
+
   (*
     /// Creates an empty stack
     pub fn new() -> Self {
@@ -64,4 +76,74 @@ Module AbstractStack.
     | {| values := []; len := _ |} => true
     | _ => false
     end.
+
+  (*
+    /// Push n copies of an item on the stack
+    pub fn push_n(&mut self, item: T, n: u64) -> Result<(), AbsStackError> {
+        if n == 0 {
+            return Ok(());
+        }
+
+        let Some(new_len) = self.len.checked_add(n) else {
+            return Err(AbsStackError::Overflow);
+        };
+        self.len = new_len;
+        match self.values.last_mut() {
+            Some((count, last_item)) if &item == last_item => {
+                debug_assert!( *count > 0);
+                *count += n
+            }
+            _ => self.values.push((n, item)),
+        }
+        Ok(())
+    }
+  *)
+
+  Definition push_n {A : Set} (item : A) (eq : A -> A -> bool) (n : Z) :
+      MS? (t A) string (Result.t unit AbsStackError.t) :=
+    if n =? 0
+    then returnS? (Result.Ok tt)
+    else
+      letS? self := readS? in
+      match U64.checked_add (len self) n with
+      | None => returnS? (Result.Err AbsStackError.Overflow)
+      | Some new_len =>
+        letS? _ := writeS? (self <| len := new_len |>) in
+        letS? _ := liftS? Lens.values (
+          letS? result := liftS?of? Vector.first_mut readS? in
+          match result with
+          | Some (count, last_item) =>
+            letS? _ := liftS?of? Vector.first_mut (
+              if eq item last_item
+              then
+                writeS? ((count + n)%Z, last_item)
+              else
+                returnS? tt
+            ) in
+            returnS? tt
+          | None =>
+            letS? values := readS? in
+            writeS? ((n, item) :: values)
+          end
+        ) in
+        returnS? (Result.Ok tt)
+      end.
+
+  (*
+    /// Pushes a single value on the stack
+    pub fn push(&mut self, item: T) -> Result<(), AbsStackError> {
+        self.push_n(item, 1)
+    }
+  *)
+
+  Definition push {A : Set} (item : A) (eq : A -> A -> bool) :
+      MS? (t A) string (Result.t unit AbsStackError.t) :=
+    push_n item eq 1.
+
+  (*
+    /// Pops a single value off the stack
+    pub fn pop(&mut self) -> Result<T, AbsStackError> {
+        self.pop_eq_n(NonZeroU64::new(1).unwrap())
+    }
+  *)
 End AbstractStack.
