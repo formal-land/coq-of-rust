2ec6231244327508ee90ea3a78ad4b8a49171859
InfiniteEchoes <128528395+InfiniteEchoes@users.noreply.github.com>
2024-08-21 00:42:33 +0800

    minor: progress

--- CoqOfRust/move_sui/simulations/move_vm_runtime/interpreter.v
+++ CoqOfRust/move_sui/simulations/move_vm_runtime/interpreter.v
@@ -8,14 +8,14 @@ Require CoqOfRust.move_sui.simulations.move_binary_format.file_format.
 Module Bytecode := file_format.Bytecode.
 
 (* TODO(progress):
+- (FOCUS)Implement `values`
 - Implement `Stack`'s `push` and `pop` operations
 - Implement `Resolver`
 - Implement `InstrRet`
 - Implement `GasMeter`? Or should we ignore it?
-- Implement `values::Locals` temporarily in this file
 *)
 
-(* NOTE: This trait doesn't have a complete implementation throughout the library
+(* NOTE: This trait doesn't have a complete implementation throughout the library. We can briefly stub it
 /// Trait that defines a generic gas meter interface, allowing clients of the Move VM to implement
 /// their own metering scheme.
 pub trait GasMeter {
@@ -139,58 +139,6 @@ pub trait GasMeter {
     fn set_profiler(&mut self, profiler: GasProfiler);
 }
 *)
-
-(* move-vm-types/src/values/values_impl.rs
-enum ValueImpl {
-    Invalid,
-
-    U8(u8),
-    U16(u16),
-    U32(u32),
-    U64(u64),
-    U128(u128),
-    U256(u256::U256),
-    Bool(bool),
-    Address(AccountAddress),
-
-    Container(Container),
-
-    ContainerRef(ContainerRef),
-    IndexedRef(IndexedRef),
-}
-*)
-Module ValueImpl.
-  Inductive t : Set :=
-  | Invalid
-  | U8 : Z -> t
-  | U16 : Z -> t
-  | U32 : Z -> t
-  | U64 : Z -> t
-  | U128 : Z -> t
-  | U256 : Z -> t
-  | Bool : bool -> t
-  (* TODO: Implement below *)
-  | Address (*(AccountAddress) *)
-  | Container (* (Container) *)
-  | ContainerRef (* (ContainerRef) *)
-  | IndexedRef (* (IndexedRef) *)
-  .
-
-End ValueImpl.
-
-(* pub struct Value(ValueImpl); *)
-Module Value.
-  Record t := { a0 : ValueImpl.t }.
-End Value.
-(* 
-/// The operand stack.
-struct Stack {
-    value: Vec<Value>,
-}
-*)
-Module Stack.
-  Record t := { value : list Value.t }.
-End Stack.
 
 (* 
 /// `Interpreter` instances can execute Move functions.
