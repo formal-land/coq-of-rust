5ecee268d4d4f086f135d11e8340fd85f44bc0ad
InfiniteEchoes <128528395+InfiniteEchoes@users.noreply.github.com>
2024-08-20 08:17:11 +0800

    minor: `loader` initial draft

---
+++ CoqOfRust/move_sui/simulations/move_vm_runtime/loader.v
@@ -0,0 +1,20 @@
+Require Import CoqOfRust.CoqOfRust.
+Require Import CoqOfRust.simulations.M.
+Require Import CoqOfRust.lib.lib.
+
+Import simulations.M.Notations.
+
+Require CoqOfRust.move_sui.simulations.move_binary_format.file_format.
+Module Bytecode := file_format.Bytecode.
+
+(* 
+// A Resolver is a simple and small structure allocated on the stack and used by the
+// interpreter. It's the only API known to the interpreter and it's tailored to the interpreter
+// needs.
+pub(crate) struct Resolver<'a> {
+    loader: &'a Loader,
+    binary: BinaryType,
+}
+*)
+Module Resolver.
+End Resolver.
