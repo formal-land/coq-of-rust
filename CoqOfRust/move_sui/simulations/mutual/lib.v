d9617ab3f8c47623d4cca99ec493996a07de299c
Antoine James <mushow@proton.me>
2024-10-04 13:52:56 +0200

    feat: Add empty and add functions to Set_ module; add empty to Map module

--- CoqOfRust/move_sui/simulations/mutual/lib.v
+++ CoqOfRust/move_sui/simulations/mutual/lib.v
@@ -12,8 +12,14 @@ Module Map.
   Parameter get : forall {K V : Set}, t K V -> K -> option V.
 
   Parameter keys : forall {K V : Set}, t K V -> list K.
+
+  Parameter empty : forall {K V : Set}, t K V.
 End Map.
 
 Module Set_.
   Parameter t : Set -> Set.
+
+  Parameter empty : forall {A : Set}, t A.
+
+  Parameter add : forall {A : Set}, A -> t A -> t A.
 End Set_.
