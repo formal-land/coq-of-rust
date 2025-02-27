9378471d2614d4a899d46b0474cc50be0b5edc43
andreadlm <delmastro2000@gmail.com>
2025-02-27 14:41:51 +0100

    run_default, start of run_code

--- CoqOfRust/revm/revm_bytecode/eof/links/body.v
+++ CoqOfRust/revm/revm_bytecode/eof/links/body.v
@@ -4,6 +4,7 @@ Require Import alloc.links.alloc.
 Require Import alloc.vec.links.mod.
 Require core.links.clone.
 Require core.links.default.
+Require Import core.links.option.
 Require Import revm.links.dependencies.
 Require Import revm.revm_bytecode.eof.links.types_section.
 Require Import revm_bytecode.eof.body.
