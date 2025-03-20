e515f80f9b9b5de9630d66314b74f4bc54df0f86
InfiniteEchoes <128528395+InfiniteEchoes@users.noreply.github.com>
2025-03-20 13:26:27 +0800

    minor: update comment

--- CoqOfRust/revm/revm_interpreter/instructions/links/block_info.v
+++ CoqOfRust/revm/revm_interpreter/instructions/links/block_info.v
@@ -102,10 +102,12 @@ Proof.
   destruct run_Block_for_Block.
   destruct beneficiary as [beneficiary [H_beneficiary run_beneficiary]].
   destruct block as [block [H_block run_block]].
-  (* TODO: Complete link for 
+  (* 
+    TODO: Complete link for 
       `push!(interpreter, host.block().beneficiary().into_word().into());`
      NOTE: how to use tactics to represent a function chain 
       like a().b().c()?... Or is it the issue with polymorphic function itself without a determined `self`?
+    TODO: find a way to import `into_word`?
   *)
   (* destruct run_beneficiary. *)
   run_symbolic.
