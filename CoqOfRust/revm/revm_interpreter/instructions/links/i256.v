640c495e62b2ae9611bbf48a3927a1ca149a01eb
InfiniteEchoes <128528395+InfiniteEchoes@users.noreply.github.com>
2025-03-21 22:12:38 +0800

    feat: Add `i256.v`

--- CoqOfRust/revm/revm_interpreter/instructions/links/i256.v
+++ CoqOfRust/revm/revm_interpreter/instructions/links/i256.v
@@ -1,1 +1,11 @@
-(* Initial stub *)
+Require Import CoqOfRust.CoqOfRust.
+Require Import CoqOfRust.links.M.
+Require Import revm.revm_interpreter.instructions.i256.
+
+(* NOTE: This function isn't associated with a trait. I wonder if this is the correct way to translate it... *)
+(* pub fn i256_div(mut first: U256, mut second: U256) -> U256  *)
+
+Global Instance Instance_IsFunction_i256_div :
+  M.IsFunction.Trait "revm::revm_interpreter::instructions::i256::i256_div" instructions.i256.i256_div.
+Admitted.
+Global Typeclasses Opaque instructions.i256.i256_div.
