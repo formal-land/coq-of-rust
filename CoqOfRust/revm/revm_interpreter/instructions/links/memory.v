51345eee066f4060263754a98c58ddd715b6f8e3
InfiniteEchoes <128528395+InfiniteEchoes@users.noreply.github.com>
2025-04-02 20:04:49 +0800

    minor: progress on `memory`

--- CoqOfRust/revm/revm_interpreter/instructions/links/memory.v
+++ CoqOfRust/revm/revm_interpreter/instructions/links/memory.v
@@ -4,6 +4,15 @@ Require Import core.links.cmp.
 Require Import revm.revm_interpreter.instructions.memory.
 Require Import revm.revm_interpreter.links.interpreter.
 Require Import revm.revm_interpreter.links.interpreter_types.
+Require Import revm.revm_context_interface.links.host.
+Require Import revm.revm_interpreter.links.gas.
+Require Import ruint.links.lib.
+
+Import Impl_Gas.
+Import links.lib.Impl_Uint.
+
+(* TODO(PROGRESS):
+*)
 
 (* pub fn mload<WIRE: InterpreterTypes, H: Host + ?Sized>(
     interpreter: &mut Interpreter<WIRE>,
@@ -12,6 +21,7 @@ Require Import revm.revm_interpreter.links.interpreter_types.
 Instance run_mload
   {WIRE H : Set} `{Link WIRE} `{Link H}
   {WIRE_types : InterpreterTypes.Types.t} `{InterpreterTypes.Types.AreLinks WIRE_types}
+  {H_types : Host.Types.t} `{Host.Types.AreLinks H_types}
   (run_InterpreterTypes_for_WIRE : InterpreterTypes.Run WIRE WIRE_types)
   (interpreter : Ref.t Pointer.Kind.MutRef (Interpreter.t WIRE WIRE_types))
   (host : Ref.t Pointer.Kind.MutRef H) :
@@ -20,6 +30,23 @@ Instance run_mload
     unit.
 Proof.
   constructor.
+  cbn.
+  eapply Run.Rewrite. {
+    progress repeat erewrite IsTraitAssociatedType_eq by apply run_InterpreterTypes_for_WIRE.
+    reflexivity.
+  }
+  destruct run_InterpreterTypes_for_WIRE.
+  destruct run_LoopControl_for_Control.
+  destruct run_StackTrait_for_Stack.
+  destruct run_RuntimeFlag_for_RuntimeFlag.
+  (* NOTE:
+  Goal stuck with the following beginning:
+  {{CoqOfRust.M.LowM.CallPrimitive
+    (CoqOfRust.M.Primitive.StateRead
+       (get_constant "core::num::MAX")) ...
+  Should I create a link on `core.links.num.`for the `MAX` constant? Or,
+  I suppose there should be a `value_MAX` related but it's too hard to find
+  *)
   run_symbolic.
 Admitted.
 
@@ -30,14 +57,28 @@ Admitted.
 Instance run_mstore
   {WIRE H : Set} `{Link WIRE} `{Link H}
   {WIRE_types : InterpreterTypes.Types.t} `{InterpreterTypes.Types.AreLinks WIRE_types}
+  {H_types : Host.Types.t} `{Host.Types.AreLinks H_types}
   (run_InterpreterTypes_for_WIRE : InterpreterTypes.Run WIRE WIRE_types)
-  (interpreter : Ref.t Pointer.Kind.MutRef (Interpreter.t WIRE WIRE_types))
+  (interpreter : Ref.t Pointer.Kind.MutRef (Interpreter.t WIRE WIRE_types)) 
   (host : Ref.t Pointer.Kind.MutRef H) :
   Run.Trait
     instructions.memory.mstore [] [ Φ WIRE; Φ H ] [ φ interpreter; φ host ]
     unit.
 Proof.
   constructor.
+  cbn.
+  eapply Run.Rewrite. {
+    progress repeat erewrite IsTraitAssociatedType_eq by apply run_InterpreterTypes_for_WIRE.
+    reflexivity.
+  }
+  destruct run_InterpreterTypes_for_WIRE.
+  destruct run_LoopControl_for_Control.
+  destruct run_StackTrait_for_Stack.
+  destruct run_RuntimeFlag_for_RuntimeFlag.
+  (* 
+  TODO:
+  - get_constant core::num::MAX?
+  *)
   run_symbolic.
 Admitted.
 
@@ -48,6 +89,7 @@ Admitted.
 Instance run_mstore8
   {WIRE H : Set} `{Link WIRE} `{Link H}
   {WIRE_types : InterpreterTypes.Types.t} `{InterpreterTypes.Types.AreLinks WIRE_types}
+  {H_types : Host.Types.t} `{Host.Types.AreLinks H_types}
   (run_InterpreterTypes_for_WIRE : InterpreterTypes.Run WIRE WIRE_types)
   (interpreter : Ref.t Pointer.Kind.MutRef (Interpreter.t WIRE WIRE_types))
   (host : Ref.t Pointer.Kind.MutRef H) :
@@ -56,7 +98,20 @@ Instance run_mstore8
     unit.
 Proof.
   constructor.
+  cbn.
+  eapply Run.Rewrite. {
+    progress repeat erewrite IsTraitAssociatedType_eq by apply run_InterpreterTypes_for_WIRE.
+    reflexivity.
+  }
+  destruct run_InterpreterTypes_for_WIRE.
+  destruct run_LoopControl_for_Control.
+  destruct run_StackTrait_for_Stack.
+  destruct run_RuntimeFlag_for_RuntimeFlag.
   run_symbolic.
+  (* 
+  TODO:
+  - ...
+  *)
 Admitted.
 
 (* pub fn msize<WIRE: InterpreterTypes, H: Host + ?Sized>(
@@ -66,6 +121,7 @@ Admitted.
 Instance run_msize
   {WIRE H : Set} `{Link WIRE} `{Link H}
   {WIRE_types : InterpreterTypes.Types.t} `{InterpreterTypes.Types.AreLinks WIRE_types}
+  {H_types : Host.Types.t} `{Host.Types.AreLinks H_types}
   (run_InterpreterTypes_for_WIRE : InterpreterTypes.Run WIRE WIRE_types)
   (interpreter : Ref.t Pointer.Kind.MutRef (Interpreter.t WIRE WIRE_types))
   (host : Ref.t Pointer.Kind.MutRef H) :
@@ -74,6 +130,15 @@ Instance run_msize
     unit.
 Proof.
   constructor.
+  cbn.
+  eapply Run.Rewrite. {
+    progress repeat erewrite IsTraitAssociatedType_eq by apply run_InterpreterTypes_for_WIRE.
+    reflexivity.
+  }
+  destruct run_InterpreterTypes_for_WIRE.
+  destruct run_LoopControl_for_Control.
+  destruct run_StackTrait_for_Stack.
+  destruct run_RuntimeFlag_for_RuntimeFlag.
   run_symbolic.
 Admitted.
 
