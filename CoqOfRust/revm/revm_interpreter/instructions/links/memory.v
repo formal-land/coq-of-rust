a6708456a448af73a76a30a2df85a2788553de8c
InfiniteEchoes <128528395+InfiniteEchoes@users.noreply.github.com>
2025-04-04 13:46:15 +0800

    minor: progress on `memory.v`

--- CoqOfRust/revm/revm_interpreter/instructions/links/memory.v
+++ CoqOfRust/revm/revm_interpreter/instructions/links/memory.v
@@ -1,18 +1,19 @@
 Require Import CoqOfRust.CoqOfRust.
 Require Import CoqOfRust.links.M.
 Require Import core.links.cmp.
+Require Import core.num.links.mod.
 Require Import revm.revm_interpreter.instructions.memory.
 Require Import revm.revm_interpreter.links.interpreter.
 Require Import revm.revm_interpreter.links.interpreter_types.
 Require Import revm.revm_context_interface.links.host.
 Require Import revm.revm_interpreter.links.gas.
+Require Import revm.revm_interpreter.gas.links.constants.
+Require Import revm.revm_interpreter.interpreter.links.shared_memory.
 Require Import ruint.links.lib.
 
+Import num.links.mod.Impl_usize.
 Import Impl_Gas.
 Import links.lib.Impl_Uint.
-
-(* TODO(PROGRESS):
-*)
 
 (* pub fn mload<WIRE: InterpreterTypes, H: Host + ?Sized>(
     interpreter: &mut Interpreter<WIRE>,
@@ -23,6 +24,7 @@ Instance run_mload
   {WIRE_types : InterpreterTypes.Types.t} `{InterpreterTypes.Types.AreLinks WIRE_types}
   {H_types : Host.Types.t} `{Host.Types.AreLinks H_types}
   (run_InterpreterTypes_for_WIRE : InterpreterTypes.Run WIRE WIRE_types)
+  (* (run_Host_for_H : Host.Run H H_types) *)
   (interpreter : Ref.t Pointer.Kind.MutRef (Interpreter.t WIRE WIRE_types))
   (host : Ref.t Pointer.Kind.MutRef H) :
   Run.Trait
@@ -33,19 +35,20 @@ Proof.
   cbn.
   eapply Run.Rewrite. {
     progress repeat erewrite IsTraitAssociatedType_eq by apply run_InterpreterTypes_for_WIRE.
+    (* progress repeat erewrite IsTraitAssociatedType_eq by apply run_Host_for_H. *)
     reflexivity.
   }
   destruct run_InterpreterTypes_for_WIRE.
   destruct run_LoopControl_for_Control.
   destruct run_StackTrait_for_Stack.
   destruct run_RuntimeFlag_for_RuntimeFlag.
-  (* NOTE:
-  Goal stuck with the following beginning:
-  {{CoqOfRust.M.LowM.CallPrimitive
-    (CoqOfRust.M.Primitive.StateRead
-       (get_constant "core::num::MAX")) ...
-  Should I create a link on `core.links.num.`for the `MAX` constant? Or,
-  I suppose there should be a `value_MAX` related but it's too hard to find
+  (* NOTE: Current goal head being like:
+  {{let- v1
+  := (let- v1
+      := (let- v1
+          := BinOp.ne (Integer.IsLink.(φ) value4)
+               (Value.Integer IntegerKind.U64 0)
+          in ...
   *)
   run_symbolic.
 Admitted.
