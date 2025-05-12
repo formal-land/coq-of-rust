c669f5605cfb53763db40980af203ed0341d593e
InfiniteEchoes <128528395+InfiniteEchoes@users.noreply.github.com>
2025-05-12 23:50:30 +0800

    minor: progress on `air`

--- CoqOfRust/plonky3/blake3_air/link/air.v
+++ CoqOfRust/plonky3/blake3_air/link/air.v
@@ -21,96 +21,95 @@ End Blake3Air.
 
 (* 
 TODO:
-- Fill in correct Self type and maybe find more references
-- Check `AirBuilder`
+- Fill in correct Self type and find more references on `block` and `interpreter`...
 *)
 
 Module Impl_Blake3Air.
-(* 
-impl Blake3Air {
-    pub fn generate_trace_rows<F: PrimeField64>(
-        &self,
-        num_hashes: usize,
-        extra_capacity_bits: usize,
-    ) -> RowMajorMatrix<F>
-*)
-Instance run_generate_trace_rows
-  {F : Set} `{Link F} 
-  (* NOTE: On `PrimeField64` Side I think there are no needs for F_types? *)
-  (* {F_types : PrimeField64.Types.t} *)
-  {run_PrimeField64_for_F : PrimeField64.Run F}
-  (* TODO: figure out the `Self` below and refer to `interpreter` or `block` *)
-  (self : Self) (num_hashes : Usize.t) (extra_capacity_bits : Usize.t) :
-  Run.Trait
-    blake3_air.air.generate_trace_rows [] [ Φ F ] [ φ num_hashes; φ extra_capacity_bits ]
-    (RowMajorMatrix.t F).
-Proof.
-  constructor.
-  run_symbolic.
-Admitted.
-
-(* 
-fn quarter_round_function<AB: AirBuilder>(
-        &self,
-        builder: &mut AB,
-        trace: &QuarterRound<<AB as AirBuilder>::Var, <AB as AirBuilder>::Expr>,
-    )
-*)
-Instance run_quarter_round_function
-  {AB : Set} `{Link AB} 
-  {run_PrimeField64_for_F : PrimeField64.Run F}
-  (self : Self) 
-  (builder : Ref.t Pointer.Kind.MutRef AB) 
-  (* TODO: translate `trace: &QuarterRound<<AB as AirBuilder>::Var, <AB as AirBuilder>::Expr>` *)
-  (trace : Set)
-  :
-  Run.Trait
-    blake3_air.air.quarter_round_function [] [ Φ AB ] [ φ builder; φ trace ]
-    unit.
-Proof.
-  constructor.
-  run_symbolic.
-Admitted.
-
-(* 
-const fn full_round_to_column_quarter_round<'a, T: Copy, U>(
-    &self,
-    input: &'a Blake3State<T>,
-    round_data: &'a FullRound<T>,
-    m_vector: &'a [[U; 2]; 16],
-    index: usize,
-) -> QuarterRound<'a, T, U>
-*)
-
-(* 
-    const fn full_round_to_diagonal_quarter_round<'a, T: Copy, U>(
-        &self,
-        round_data: &'a FullRound<T>,
-        m_vector: &'a [[U; 2]; 16],
-        index: usize,
-    ) -> QuarterRound<'a, T, U> 
-*)
-
-(* 
-fn verify_round<AB: AirBuilder>(
-        &self,
-        builder: &mut AB,
-        input: &Blake3State<AB::Var>,
-        round_data: &FullRound<AB::Var>,
-        m_vector: &[[AB::Expr; 2]; 16],
-    )
-*)
-
-(* 
-  Instance run_STOP :
+  (* 
+  impl Blake3Air {
+      pub fn generate_trace_rows<F: PrimeField64>(
+          &self,
+          num_hashes: usize,
+          extra_capacity_bits: usize,
+      ) -> RowMajorMatrix<F>
+  *)
+  Instance run_generate_trace_rows
+    {F : Set} `{Link F} 
+    (* NOTE: On `PrimeField64` Side I think there are no needs for F_types? *)
+    (* {F_types : PrimeField64.Types.t} *)
+    {run_PrimeField64_for_F : PrimeField64.Run F}
+    (* TODO: figure out the `Self` below and refer to `interpreter` or `block` *)
+    (self : Self) (num_hashes : Usize.t) (extra_capacity_bits : Usize.t) :
     Run.Trait
-      opcode.Impl_revm_bytecode_opcode_OpCode.value_STOP [] [] []
-      (Ref.t Pointer.Kind.Raw OpCode.t).
+      blake3_air.air.generate_trace_rows [] [ Φ F ] [ φ num_hashes; φ extra_capacity_bits ]
+      (RowMajorMatrix.t F).
   Proof.
     constructor.
     run_symbolic.
-  Defined.
-*)
+  Admitted.
+
+  (* 
+  fn quarter_round_function<AB: AirBuilder>(
+          &self,
+          builder: &mut AB,
+          trace: &QuarterRound<<AB as AirBuilder>::Var, <AB as AirBuilder>::Expr>,
+      )
+  *)
+  Instance run_quarter_round_function
+    {AB : Set} `{Link AB} 
+    {run_PrimeField64_for_F : PrimeField64.Run F}
+    (self : Self) 
+    (builder : Ref.t Pointer.Kind.MutRef AB) 
+    (* TODO: translate `trace: &QuarterRound<<AB as AirBuilder>::Var, <AB as AirBuilder>::Expr>` *)
+    (trace : Set)
+    :
+    Run.Trait
+      blake3_air.air.quarter_round_function [] [ Φ AB ] [ φ builder; φ trace ]
+      unit.
+  Proof.
+    constructor.
+    run_symbolic.
+  Admitted.
+
+  (* 
+  const fn full_round_to_column_quarter_round<'a, T: Copy, U>(
+      &self,
+      input: &'a Blake3State<T>,
+      round_data: &'a FullRound<T>,
+      m_vector: &'a [[U; 2]; 16],
+      index: usize,
+  ) -> QuarterRound<'a, T, U>
+  *)
+
+  (* 
+      const fn full_round_to_diagonal_quarter_round<'a, T: Copy, U>(
+          &self,
+          round_data: &'a FullRound<T>,
+          m_vector: &'a [[U; 2]; 16],
+          index: usize,
+      ) -> QuarterRound<'a, T, U> 
+  *)
+
+  (* 
+  fn verify_round<AB: AirBuilder>(
+          &self,
+          builder: &mut AB,
+          input: &Blake3State<AB::Var>,
+          round_data: &FullRound<AB::Var>,
+          m_vector: &[[AB::Expr; 2]; 16],
+      )
+  *)
+
+  (* 
+    Instance run_STOP :
+      Run.Trait
+        opcode.Impl_revm_bytecode_opcode_OpCode.value_STOP [] [] []
+        (Ref.t Pointer.Kind.Raw OpCode.t).
+    Proof.
+      constructor.
+      run_symbolic.
+    Defined.
+  *)
 End Impl_Blake3Air.
 
 (* 
