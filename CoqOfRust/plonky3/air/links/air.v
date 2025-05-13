6d5101a53e898b5d10fca75fa2f9a67516473732
InfiniteEchoes <128528395+InfiniteEchoes@users.noreply.github.com>
2025-05-13 19:52:41 +0800

    minor: progress

--- CoqOfRust/plonky3/air/links/air.v
+++ CoqOfRust/plonky3/air/links/air.v
@@ -34,7 +34,6 @@ Module AirBuilder.
     }
     fn is_transition_window(&self, size: usize) -> Self::Expr;
 
-    /// Returns a sub-builder whose constraints are enforced only when `condition` is nonzero.
     fn when<I: Into<Self::Expr>>(&mut self, condition: I) -> FilteredAirBuilder<'_, Self> {
         FilteredAirBuilder {
             inner: self,
@@ -42,7 +41,6 @@ Module AirBuilder.
         }
     }
 
-    /// Returns a sub-builder whose constraints are enforced only when `x != y`.
     fn when_ne<I1: Into<Self::Expr>, I2: Into<Self::Expr>>(
         &mut self,
         x: I1,
@@ -51,61 +49,43 @@ Module AirBuilder.
         self.when(x.into() - y.into())
     }
 
-    /// Returns a sub-builder whose constraints are enforced only on the first row.
     fn when_first_row(&mut self) -> FilteredAirBuilder<'_, Self> {
         self.when(self.is_first_row())
     }
 
-    /// Returns a sub-builder whose constraints are enforced only on the last row.
     fn when_last_row(&mut self) -> FilteredAirBuilder<'_, Self> {
         self.when(self.is_last_row())
     }
 
-    /// Returns a sub-builder whose constraints are enforced on all rows except the last.
     fn when_transition(&mut self) -> FilteredAirBuilder<'_, Self> {
         self.when(self.is_transition())
     }
 
-    /// Returns a sub-builder whose constraints are enforced on all rows except the last `size - 1`.
     fn when_transition_window(&mut self, size: usize) -> FilteredAirBuilder<'_, Self> {
         self.when(self.is_transition_window(size))
     }
 
-    /// Assert that the given element is zero.
-    ///
-    /// Where possible, batching multiple assert_zero calls
-    /// into a single assert_zeros call will improve performance.
     fn assert_zero<I: Into<Self::Expr>>(&mut self, x: I);
 
-    /// Assert that every element of a given array is 0.
-    ///
-    /// This should be preferred over calling `assert_zero` multiple times.
     fn assert_zeros<const N: usize, I: Into<Self::Expr>>(&mut self, array: [I; N]) {
         for elem in array {
             self.assert_zero(elem);
         }
     }
 
-    /// Assert that a given array consists of only boolean values.
     fn assert_bools<const N: usize, I: Into<Self::Expr>>(&mut self, array: [I; N]) {
         let zero_array = array.map(|x| x.into().bool_check());
         self.assert_zeros(zero_array);
     }
 
-    /// Assert that `x` element is equal to `1`.
     fn assert_one<I: Into<Self::Expr>>(&mut self, x: I) {
         self.assert_zero(x.into() - Self::Expr::ONE);
     }
 
-    /// Assert that the given elements are equal.
     fn assert_eq<I1: Into<Self::Expr>, I2: Into<Self::Expr>>(&mut self, x: I1, y: I2) {
         self.assert_zero(x.into() - y.into());
     }
 
-    /// Assert that `x` is a boolean, i.e. either `0` or `1`.
-    ///
-    /// Where possible, batching multiple assert_bool calls
-    /// into a single assert_bools call will improve performance.
     fn assert_bool<I: Into<Self::Expr>>(&mut self, x: I) {
         self.assert_zero(x.into().bool_check());
     }
