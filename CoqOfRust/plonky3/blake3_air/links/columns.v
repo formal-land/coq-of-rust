13a6b64d2f8de4e0580cdd129f725add5dbfcd7f
InfiniteEchoes <128528395+InfiniteEchoes@users.noreply.github.com>
2025-05-17 22:14:37 +0800

    minor: progress on `columns`

--- CoqOfRust/plonky3/blake3_air/links/columns.v
+++ CoqOfRust/plonky3/blake3_air/links/columns.v
@@ -2,10 +2,6 @@ Require Import CoqOfRust.CoqOfRust.
 Require Import CoqOfRust.links.M.
 Require Import plonky3.blake3_air.columns.
 
-(* TODO:
-- fill in `of_ty` for structs
-*)
-
 (* 
 pub(crate) struct QuarterRound<'a, T, U> {
     pub a: &'a [T; U32_LIMBS],
@@ -49,10 +45,16 @@ Module QuarterRound.
 
   Parameter to_value : forall {T U : Set}, t T U -> Value.t.
 
-  (* Global Instance IsLink (T U : Set) : Link (t T U) := {
-    Φ := Ty.path "plonky3::blake3_air::columns::QuarterRound";
+  Global Instance IsLink (T U : Set) `{Link T} `{Link U} : Link (t T U) := {
+    Φ := Ty.apply (Ty.path "plonky3::blake3_air::columns::QuarterRound") [] [ Φ T; Φ U ];
     φ := to_value;
-  }. *)
+  }.
+
+  Definition of_ty (T_ty U_ty : Ty.t) :
+    OfTy.t T_ty -> OfTy.t U_ty ->
+    OfTy.t (Ty.apply (Ty.path "plonky3::blake3_air::columns::QuarterRound") [] [ T_ty ; U_ty ]).
+  Proof. intros [T] [U]. eapply OfTy.Make with (A := t T U). now subst. Defined.
+  Smpl Add eapply of_ty : of_ty.
 End QuarterRound.
 
 (* 
@@ -74,10 +76,16 @@ Module Blake3State.
 
   Parameter to_value : forall {T : Set}, t T -> Value.t.
 
-  (* Global Instance IsLink (T : Set) : Link (t T) := {
-    Φ := Ty.path "plonky3::blake3_air::columns::Blake3State";
+  Global Instance IsLink (T : Set) `{Link T} : Link (t T) := {
+    Φ := Ty.apply (Ty.path "plonky3::blake3_air::columns::Blake3State") [] [ Φ T ];
     φ := to_value;
-  }. *)
+  }.
+
+  Definition of_ty (T_ty : Ty.t) :
+    OfTy.t T_ty -> 
+    OfTy.t (Ty.apply (Ty.path "plonky3::blake3_air::columns::Blake3State") [] [ T_ty ]).
+  Proof. intros [T]. eapply OfTy.Make with (A := t T). now subst. Defined.
+  Smpl Add eapply of_ty : of_ty.
 End Blake3State.
 
 (* 
@@ -99,8 +107,14 @@ Module FullRound.
 
   Parameter to_value : forall {T : Set}, t T -> Value.t.
 
-  (* Global Instance IsLink (T : Set) : Link (t T) := {
-    Φ := Ty.path "plonky3::blake3_air::columns::FullRound";
+  Global Instance IsLink (T : Set) `{Link T} : Link (t T) := {
+    Φ := Ty.apply (Ty.path "plonky3::blake3_air::columns::FullRound") [] [ Φ T ];
     φ := to_value;
-  }. *)
+  }.
+
+  Definition of_ty (T_ty : Ty.t) :
+    OfTy.t T_ty -> 
+    OfTy.t (Ty.apply (Ty.path "plonky3::blake3_air::columns::FullRound") [] [ T_ty ]).
+  Proof. intros [T]. eapply OfTy.Make with (A := t T). now subst. Defined.
+  Smpl Add eapply of_ty : of_ty.
 End FullRound.
