98b144606adf5c6e6cf8b35dd8df354435562f3d
InfiniteEchoes <128528395+InfiniteEchoes@users.noreply.github.com>
2025-06-06 12:54:56 +0800

    feat: Add `generation.v` dependency

--- CoqOfRust/plonky3/blake3_air/links/generation.v
+++ CoqOfRust/plonky3/blake3_air/links/generation.v
@@ -1,1 +1,20 @@
-(* TODO: generate_trace_rows *)
+Require Import CoqOfRust.CoqOfRust.
+Require Import CoqOfRust.links.M.
+Require Import plonky3.blake3_air.air.generation.
+Require Import plonky3.field.links.field.
+Require Import plonky3.matrix.links.dense.
+
+(* 
+pub fn generate_trace_rows<F: PrimeField64>(
+    inputs: Vec<[u32; 24]>,
+    extra_capacity_bits: usize,
+) -> RowMajorMatrix<F> {
+*)
+Instance run_generate_trace_rows
+  {F : Set} `{Link F}
+  {run_PrimeField64_for_F : PrimeField64.Run F}
+  (inputs : array.t U32.t {| Integer.value := 2 |})
+  (extra_capacity_bits : Usize.t) :
+  Run.Trait
+    air.generation.generate_trace_rows [] [ Φ F; ] [ φ inputs; φ extra_capacity_bits ]
+    RowMajorMatrix.t F.
