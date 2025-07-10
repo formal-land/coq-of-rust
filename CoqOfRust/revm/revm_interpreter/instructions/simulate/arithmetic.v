1816b84497a5de7299bb97a66cbac84496e80cc9
Guillaume Claret <dev@clarus.me>
2025-07-10 15:42:31 +0200

    simulate: add macro definitions for ADD

--- CoqOfRust/revm/revm_interpreter/instructions/simulate/arithmetic.v
+++ CoqOfRust/revm/revm_interpreter/instructions/simulate/arithmetic.v
@@ -13,15 +13,84 @@ Require Import revm.revm_interpreter.simulate.gas.
 Require Import revm.revm_interpreter.simulate.interpreter_types.
 Require Import ruint.simulate.add.
 
+Definition gas_macro {WIRE : Set} `{Link WIRE}
+    {WIRE_types : InterpreterTypes.Types.t} `{InterpreterTypes.Types.AreLinks WIRE_types}
+    (IInterpreterTypes : InterpreterTypes.C WIRE_types)
+    (interpreter : Interpreter.t WIRE WIRE_types)
+    (cost : U64.t)
+    (k : Interpreter.t WIRE WIRE_types -> Interpreter.t WIRE WIRE_types) :
+    Interpreter.t WIRE WIRE_types :=
+  let gas :=
+    IInterpreterTypes
+        .(InterpreterTypes.Loop)
+        .(Loop.gas)
+        .(RefStub.projection)
+      interpreter.(Interpreter.control) in
+  match Impl_Gas.record_cost gas cost with
+  | None =>
+    let control :=
+      IInterpreterTypes
+          .(InterpreterTypes.Loop)
+          .(Loop.set_instruction_result)
+        interpreter.(Interpreter.control)
+        instruction_result.InstructionResult.OutOfGas in
+    interpreter
+      <| Interpreter.control := control |>
+  | Some gas =>
+    let control :=
+      IInterpreterTypes
+          .(InterpreterTypes.Loop)
+          .(Loop.gas)
+          .(RefStub.injection)
+        interpreter.(Interpreter.control) gas in
+    let interpreter :=
+      interpreter
+        <| Interpreter.control := control |> in
+    k interpreter
+  end.
+
+Definition popn_top_macro {WIRE : Set} `{Link WIRE}
+    {WIRE_types : InterpreterTypes.Types.t} `{InterpreterTypes.Types.AreLinks WIRE_types}
+    (IInterpreterTypes : InterpreterTypes.C WIRE_types)
+    (interpreter : Interpreter.t WIRE WIRE_types)
+    (N : Usize.t)
+    (k :
+      array.t aliases.U256.t N ->
+      RefStub.t WIRE_types.(InterpreterTypes.Types.Stack) aliases.U256.t ->
+      Interpreter.t WIRE WIRE_types ->
+      Interpreter.t WIRE WIRE_types
+    ) :
+    Interpreter.t WIRE WIRE_types :=
+  let stack := interpreter.(Interpreter.stack) in
+  let (result, stack) :=
+    IInterpreterTypes
+        .(InterpreterTypes.Stack)
+        .(Stack.popn_top)
+      N stack in
+  match result with
+  | Some (arr, top) =>
+    let interpreter :=
+      interpreter
+        <| Interpreter.stack := stack |> in
+    k arr top interpreter
+  | None =>
+    let control :=
+      IInterpreterTypes.(InterpreterTypes.Loop).(Loop.set_instruction_result)
+        interpreter.(Interpreter.control)
+        instruction_result.InstructionResult.StackUnderflow in
+    interpreter
+      <| Interpreter.control := control |>
+      <| Interpreter.stack := stack |>
+  end.
+
 Lemma add_eq
     {WIRE H : Set} `{Link WIRE} `{Link H}
     {WIRE_types : InterpreterTypes.Types.t} `{InterpreterTypes.Types.AreLinks WIRE_types}
     {H_types : Host.Types.t} `{Host.Types.AreLinks H_types}
     (run_InterpreterTypes_for_WIRE : InterpreterTypes.Run WIRE WIRE_types)
-    (ILoop : Loop.C WIRE_types)
-    (LoopEq : Loop.Eq.t WIRE WIRE_types run_InterpreterTypes_for_WIRE ILoop)
-    (IStack : Stack.C WIRE_types)
-    (StackEq : Stack.Eq.t WIRE WIRE_types run_InterpreterTypes_for_WIRE IStack)
+    (IInterpreterTypes : InterpreterTypes.C WIRE_types)
+    (InterpreterTypesEq :
+      InterpreterTypes.Eq.t WIRE WIRE_types run_InterpreterTypes_for_WIRE IInterpreterTypes)
     (interpreter : Interpreter.t WIRE WIRE_types)
     (_host : H) :
   let ref_interpreter := make_ref 0 in
@@ -33,45 +102,24 @@ Lemma add_eq
     (
       Output.Success tt,
       (
-        let gas := ILoop.(Loop.gas).(RefStub.projection) interpreter.(Interpreter.control) in
-        match Impl_Gas.record_cost gas constants.VERYLOW with
-        | None =>
-          let control :=
-            ILoop.(Loop.set_instruction_result)
-              interpreter.(Interpreter.control)
-              instruction_result.InstructionResult.OutOfGas in
+        gas_macro IInterpreterTypes interpreter constants.VERYLOW (fun interpreter =>
+        popn_top_macro IInterpreterTypes interpreter {| Integer.value := 1 |} (fun arr top interpreter =>
+          let '{| ArrayPair.x := x1 |} := arr.(array.value) in
+          let x2 := top.(RefStub.projection) interpreter.(Interpreter.stack) in
+          let stack :=
+            top.(RefStub.injection)
+              interpreter.(Interpreter.stack) (Impl_Uint.wrapping_add x1 x2) in
           interpreter
-            <| Interpreter.control := control |>
-        | Some gas =>
-          let control := ILoop.(Loop.gas).(RefStub.injection) interpreter.(Interpreter.control) gas in
-          let stack := interpreter.(Interpreter.stack) in
-          let (result, stack) := IStack.(Stack.popn_top) {| Integer.value := 1 |} stack in
-          match result with
-          | Some (arr, top) =>
-            let '{| ArrayPair.x := x1 |} := arr.(array.value) in
-            let x2 := top.(RefStub.projection) stack in
-            let stack := top.(RefStub.injection) stack (Impl_Uint.wrapping_add x1 x2) in
-            interpreter
-              <| Interpreter.control := control |>
-              <| Interpreter.stack := stack |>
-          | None =>
-            let control :=
-              ILoop.(Loop.set_instruction_result)
-                control
-                instruction_result.InstructionResult.StackUnderflow in
-            interpreter
-              <| Interpreter.control := control |>
-              <| Interpreter.stack := stack |>
-          end
-        end,
+            <| Interpreter.stack := stack |>
+        )),
         (_host, tt)
       )
     )
   }}.
 Proof.
   intros.
-  destruct LoopEq, StackEq.
-  unfold run_add, StackM.eval_f, StackM.eval, evaluate.
+  destruct InterpreterTypesEq as [[] []].
+  unfold run_add, gas_macro, popn_top_macro, StackM.eval_f, StackM.eval, evaluate.
   eapply Run.Call. {
     apply gas.
   }
@@ -88,7 +136,7 @@ Proof.
     eapply Run.Call. {
       apply popn_top.
     }
-    destruct IStack.(Stack.popn_top) as [[[? ?]|] ?].
+    destruct IInterpreterTypes.(InterpreterTypes.Stack).(Stack.popn_top) as [[[? ?]|] ?].
     { get_can_access.
       eapply Run.Call. {
         rewrite Impl_Uint.wrapping_add_eq.
