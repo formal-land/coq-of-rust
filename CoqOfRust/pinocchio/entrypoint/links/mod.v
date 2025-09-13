f11331a22c955ae47577e50ae4ea090ad991dc10
romefeller <alexgrcol@hotmail.com>
2025-09-13 10:47:44 -0300

    Introducing entrypoint/mod.v

---
+++ CoqOfRust/pinocchio/entrypoint/links/mod.v
@@ -0,0 +1,52 @@
+Require Import CoqOfRust.CoqOfRust.
+Require Import CoqOfRust.links.M.
+Require Import core.links.array.
+Require Import pinocchio.links.account_info.
+Require Import pinocchio.entrypoint.mod.
+
+Module entrypoint.
+  Module deserialize.
+
+    Parameter MAX_ACCOUNTS : Usize.t.
+
+    Definition Self : Set := (Ref.t Pointer.Kind.Raw U8.t *
+                              Usize.t *
+                              Ref.t Pointer.Kind.Ref (list U8.t))%type.
+
+    Instance run_deserialize
+      (input : Ref.t Pointer.Kind.Raw U8.t)
+      (accounts : Ref.t Pointer.Kind.Ref (array.t AccountInfo.t MAX_ACCOUNTS)) :
+      Run.Trait
+        pinocchio.entrypoint.mod.entrypoint.deserialize
+        [] []
+        [ φ input; φ accounts ]
+        Self.
+    Proof.
+      constructor.
+      run_symbolic.
+      admit. 
+    Admitted.
+  End deserialize.
+
+  Module parse.
+    Parameter MAX_ACCOUNTS : Usize.t.
+
+    Definition Self : Set := (Ref.t Pointer.Kind.Raw U8.t *
+                              Usize.t *
+                              Ref.t Pointer.Kind.Ref (list U8.t))%type.
+
+    Instance run
+      (input : Ref.t Pointer.Kind.Raw U8.t)
+      (accounts : Ref.t Pointer.Kind.Ref (array.t AccountInfo.t MAX_ACCOUNTS)) :
+      Run.Trait
+        pinocchio.entrypoint.mod.entrypoint.parse
+        [] []
+        [ φ input; φ accounts ]
+        Self.
+    Proof.
+      constructor.
+      run_symbolic.
+      admit. 
+    Admitted.
+  End parse.
+End entrypoint.
