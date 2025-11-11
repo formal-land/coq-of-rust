6979dba1fd75102dcd255cb1fa5514ea07250ea0
Guillaume Claret <dev@clarus.me>
2025-11-11 15:40:30 +0100

    simulate: add one more example

--- CoqOfRust/examples/default/examples/custom/simulate/loops_free.v
+++ CoqOfRust/examples/default/examples/custom/simulate/loops_free.v
@@ -24,3 +24,40 @@ Proof.
     destruct (_ <? _)
   ).
 Qed.
+Global Opaque run_max2.
+
+Definition abs_i32 (x : I32.t) : I32.t :=
+  if x.(Integer.value) <? 0 then
+    if x.(Integer.value) =? - 2^31 then
+      x
+    else
+      {| Integer.value := - x.(Integer.value) |}
+  else
+    x.
+
+Lemma abs_i32_eq (Stack : Stack.t) (stack : Stack.to_Set Stack)
+    (x : I32.t) :
+  {{
+    SimulateM.eval_f (run_abs_i32 x) stack 🌲
+    (Output.Success (abs_i32 x), stack)
+  }}.
+Proof.
+  unfold abs_i32; cbn.
+  eapply Run.Call. {
+    apply Run.Pure.
+  }
+  cbn.
+  eapply Run.Call. {
+    apply Run.Pure.
+  }
+  cbn.
+  destruct (_ <? 0); cbn.
+  { eapply Run.Call. {
+      apply Run.Pure.
+    }
+    cbn.
+    destruct (_ =? _); cbn; apply Run.Pure.
+  }
+  { apply Run.Pure. }
+Qed.
+Global Opaque run_max2.
